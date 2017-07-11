114ba08d3f295cd0061b4102b26f2c2d373a78a0
arlinkeo <arlinkeo@gmail.com>
2017-07-11 23:00:18 +0200

    corr. normalized expr.

--- braaklabelcorr.R
+++ braaklabelcorr.R
@@ -3,20 +3,27 @@
 options(stringsAsFactors = FALSE)
 setwd("C:/Users/dkeo/surfdrive/Parkinson")
 
-load("../polyQ_coexpression/resources/BrainExpr.RData")
+load("../ABA_Rdata/BrainExpr.RData")
 donorNames <- names(brainExpr)
 names(donorNames) <- donorNames
+load("../ABA_Rdata/BrainExprNorm.RData")
 load("resources/braakLabels.RData")
 
-# Correlate gene expression to Braak labels for each brain (without non-Braak region)
-geneLabelCor <- sapply(donorNames, function(d){
-  labels <- braakLabels[[d]]
-  nbCols <- labels == 0
-  labels <- labels[!nbCols]
-  expr <- brainExpr[[d]][, !nbCols]
-  apply(expr, 1, function(g){# corr. each gene
-    g <- unlist(g)
-    cor(g, as.numeric(labels)) # Pearson's r by default
+braak.cor <- function(dataList) {
+  sapply(donorNames, function(d){
+    labels <- braakLabels[[d]]
+    nbCols <- labels == 0
+    labels <- labels[!nbCols]
+    expr <- dataList[[d]][, !nbCols]
+    apply(expr, 1, function(g){# corr. each gene
+      g <- unlist(g)
+      cor(g, as.numeric(labels)) # Pearson's r by default
+    })
   })
-})
+}
+
+# Correlate gene expression to Braak labels for each brain (without non-Braak region)
+geneLabelCor <- braak.cor(brainExpr)
+geneLabelCorNorm <- braak.cor(brainExprNorm)
 save(geneLabelCor, file = "resources/geneLabelCor.RData")
+save(geneLabelCorNorm, file = "resources/geneLabelCorNorm.RData")
