35e3b9c4c88a3d033e61e14eef8cad716460795c
arlinkeo <arlinkeo@gmail.com>
2017-08-21 15:29:27 +0200

    as char

--- base_script.R
+++ base_script.R
@@ -6,7 +6,7 @@ probeInfo <- read.csv("../ABA_human_processed/probe_info_2014-11-11.csv")
 
 entrezId2Name <- function (x) {probeInfo$gene_symbol[match(x, probeInfo$entrez_id)]} #Input is vector
 
-name2EntrezId <- function (x) {probeInfo$entrez_id[match(x, probeInfo$gene_symbol)]} #Input is vector
+name2EntrezId <- function (x) {as.character(probeInfo$entrez_id[match(x, probeInfo$gene_symbol)])} #Input is vector
 
 
 donorNames <- c("donor9861", "donor10021", "donor12876", "donor14380", "donor15496", "donor15697")
