11a75cf976d30bb46ec0ed7fde8abdc6d9f5115b
arlinkeo <arlinkeo@gmail.com>
2017-08-31 14:56:16 +0200

    also get pvalue from estimated beta

--- sumEffectSize.R
+++ sumEffectSize.R
@@ -22,16 +22,18 @@ sumTable <- function(g){
   print(paste(g, entrezId2Name(g)))
   lapply(braakNames, function(bs){
     donorList <- diffGenesList[[bs]]
-    tab <- as.data.frame(t(sapply(donorList, function(t) unlist(t[g, ]))))
+    tab <- as.data.frame(t(sapply(donorList, function(t) unlist(t[g, ]))))# get gene row for each donor
     tab$meanDiff <- unlist(tab$meanB) - unlist(tab$meanNB)# mean difference
     donors <- sapply(rownames(tab), function(n){ paste0("Donor ", unlist(strsplit(n, split = "donor"))[2])})
     tab$donor <- donors
     tab$size <- sizes[, bs]
     tab$varDiff <- (tab$varB / tab$size) + (tab$varNB / sizesNB) # variance of mean difference
-    tab <- tab[, c("lower95", "upper95", "meanDiff", "varDiff", "donor", "size")]
+    tab$pvalue <- tab$benjamini_hochberg
+    tab <- tab[, c("lower95", "upper95", "meanDiff", "varDiff", "donor", "size", "pvalue")]
     sumEffect <- rma(tab$meanDiff, tab$varDiff, method = "DL") # Summary effect size
     tab$weight <- round(weights(sumEffect), digits = 2)
-    tab <- rbind(tab, 'SummaryEff' = list(sumEffect$ci.lb, sumEffect$ci.ub, sumEffect$beta, sumEffect$se^2, "Summary", sum(tab$size), sum(tab$weight)))
+    tab <- rbind(tab, 'SummaryEff' = list(sumEffect$ci.lb, sumEffect$ci.ub, sumEffect$beta, sumEffect$se^2, 
+                                          "Summary", sum(tab$size), sumEffect$pval, sum(tab$weight)))
     tab$isSum <- tab$donor == "Summary"
     tab$braak <- bs
     tab
