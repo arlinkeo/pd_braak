d8e368cb2db4f68c014d48dc403cbe6e795b22ca
arlinkeo <arlinkeo@gmail.com>
2017-10-27 21:50:27 +0200

    old stuff

--- diff_expr_davidgo.R
+++
@@ -1,7 +0,0 @@
-# Differentially expressed genes in DAVID
-
-setwd("C:/Users/dkeo/surfdrive/Parkinson")
-options(stringsAsFactors = FALSE)
-library("RDAVIDWebService")
-
-load("resources/diffGenesBraak.RData") # for looking up p-value (2 diff. data structures)
