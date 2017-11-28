96e09b616ab1ba8d8cd97ceb979525cfb6b85436
arlinkeo <arlinkeo@gmail.com>
2017-11-28 19:39:34 +0100

    Hmisc package rcorr function

--- gene_coexpr.R
+++ gene_coexpr.R
@@ -1,7 +1,7 @@
 # Gene co-expression in whole Braak region
 setwd("C:/Users/dkeo/surfdrive/Parkinson")
 
-library("psych")
+library("Hmisc")
 
 source("PD/base_script.R")
 load("../ABA_Rdata/BrainExprNorm.RData")
@@ -20,11 +20,13 @@ braakExpr <- lapply(donorNames, function(d){
   brainExprNorm[[d]][, labels]
 })
 sapply(braakExpr, dim)
+remove(brainExprNorm)
 
-# Gene co-expression in Braak samples, for each donor
+# p-values of correlations
 gene_coexpr <- lapply(donorNames, function(d){
   expr <- braakExpr[[d]]
-  x=corr.test(t(expr), adjust = "BH")
+  m <- rcorr(t(expr), type = "pearson") #List with correlation-, size-, and p-value-matrix
+  m$n <- m$n[1, 1]
+  m
 })
 save(gene_coexpr, file = "gene_coexpr.Rdata")
-
