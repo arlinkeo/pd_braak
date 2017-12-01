9141f022a1daeb2c25d713ed7ff13d34528524d7
arlinkeo <arlinkeo@gmail.com>
2017-12-01 13:38:07 +0100

    summary effect estimted based on fisher transformed correlation

--- braaklabelcorr.R
+++ braaklabelcorr.R
@@ -7,6 +7,9 @@ library(metafor)
 load("../ABA_Rdata/BrainExprNorm.RData")
 load("resources/braakLabels.RData")
 
+#Functions
+back.transform <- dget("PD/back.transform.R")
+
 braak.cor <- function(dataList, stat) {
   sapply(donorNames, function(d){
     labels <- braakLabels[[d]]
@@ -30,34 +33,33 @@ geneLabelPVal <- braak.cor(brainExprNorm, "p.value")
 genes <- rownames(brainExprNorm$donor9861)
 braakSize <- sapply(braakLabels, function(x){sum(sapply(c(1:6), function(b){sum(x==b)}))})
 
-# # Transform correlations to Fisher's z-scale and get corresponding sampling variances and confidence intervals
-# geneLabelZscore <- apply(geneLabelCor, 1, function(r){
-#   t <- escalc(measure = "ZCOR", ri = r, ni = braakSize)
-#   t <- summary(t)
-#   t <- t[, c(1,2,5,6)]
-#   colnames(t) <- c("z", "variance", "lower95", "upper95")
-#   t
-# })
-#Summary correlation and corresponding sampling variances and confidence intervals
-geneLabelRawCor <- apply(geneLabelCor, 1, function(r){
-  t <- escalc(measure = "COR", ri = r, ni = braakSize)
+# Transform correlations to Fisher's z-scale and get corresponding sampling variances and confidence intervals
+geneLabelZscore <- apply(geneLabelCor, 1, function(r){
+  t <- escalc(measure = "ZCOR", ri = r, ni = braakSize)
   t <- summary(t)
   t <- t[, c(1,2,5,6)]
-  colnames(t) <- c("z", "variance", "lower95", "upper95")
+  colnames(t) <- c("r", "variance", "lower95", "upper95")
   t
 })
 
 
 # Get summary correlation
-summaryCorr <- lapply(genes, function(g){
-  tab <- geneLabelRawCor[[g]]
+summaryCor <- lapply(genes, function(g){
+  tab <- geneLabelZscore[[g]]
   pvalue <- geneLabelPVal[g,]
   donors <- sapply(rownames(tab), function(n){ gsub("donor", "Donor ", n)})
-  summary <- rma(tab$z, tab$variance, method = "DL")
+  summary <- rma(tab$r, tab$variance, method = "DL")
   weight <- round(weights(summary), digits = 2)
   tab <- cbind(donors, tab, pvalue, braakSize, weight)
   rbind(tab, 'summary' = list("Summary", summary$beta, summary$se^2 , summary$ci.lb, summary$ci.ub, 
                               summary$pval, sum(braakSize), sum(weight)))
 })
-names(summaryCorr) <- genes
-save(summaryCorr, file = "resources/summaryCorr.RData")
+names(summaryCor) <- genes
+
+# Back transform correlations, variances and confidence intervals
+summaryLabelCorr <- lapply(summaryCor, function(t){
+  t[, c("r", "variance", "lower95", "upper95")] <- back.transform(t[, c("r", "variance", "lower95", "upper95")])
+  t
+})
+
+save(summaryLabelCorr, file = "resources/summaryLabelCorr.RData")
