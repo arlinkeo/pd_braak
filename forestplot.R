74bcf76bd52b3202c53be6e6aa50718a9d50a998
arlinkeo <arlinkeo@gmail.com>
2017-12-10 20:10:41 +0100

    forestplot nonbraak

--- forestplot.R
+++ forestplot.R
@@ -5,7 +5,7 @@ library(ggplot2)
 library(gridExtra)
 
 source("PD/base_script.R")
-load("resources/summaryMeanDiff.RData")
+load("resources/summaryDiffExprNonBraak.RData")
 
 gene <- "GCH1"
 geneId <- name2EntrezId(gene)
@@ -17,18 +17,16 @@ theme.grid <- theme(panel.background = element_blank(), panel.grid = element_bla
 ####################################
 # Forest plot expression change
 
-nonBraakRef <- "nonBraakA1"
+nonBraakRef <- "nonBraakA2"
 
 #Get values for a gene to plot
 braakList <- summaryMeanDiff[[nonBraakRef]][[geneId]]
-braakList$`braak1-3` <- NULL
-braakList$`braak4-6` <- NULL
+braakList <- braakList[braakNamesMerged1]
 braakList <- lapply(names(braakList), function(b){
   df <- braakList[[b]]
   df$braak <- gsub("braak", "Braak ", b)
   df
 })
-names(braakList) <- c(braakNames, braakNamesMerged1)
 
 # Combine df's for each braak stage into one df
 tab <- do.call(rbind, braakList)
@@ -37,10 +35,11 @@ tab$donors <- factor(tab$donors, levels = unique(rev(tab$donors)))
 tab$braak <- factor(tab$braak, levels = unique(tab$braak))
 # Side table info
 tab$rmd <- paste0(round(tab$meanDiff, digits = 2), " (", round(tab$lower95, digits = 2), ", ", round(tab$upper95, digits = 2), ")")
+tab$benjamini_hochberg <- signif(tab$benjamini_hochberg, digits = 2)
 tab$is.summary <- tab$donors == "Summary"
 
-x.max <- round(max(tab$upper95), digits = 2)
-x.min <- round(min(tab$lower95), digits = 2)
+# x.max <- round(max(tab$upper95), digits = 2)
+# x.min <- round(min(tab$lower95), digits = 2)
 
 # Basic ggplot grid for braak stages
 facet.braak <- facet_grid(braak~., scales = 'free', space = 'free', switch = "y")
@@ -48,8 +47,8 @@ facet.braak <- facet_grid(braak~., scales = 'free', space = 'free', switch = "y"
 # Forest plot
 fp <- ggplot(data = tab, aes(meanDiff, donors)) 
 
-x.positions <- c(0.25, 0.55, 0.65) + x.max#c(1:3)
-colLabels <- data.frame(x = x.positions, y = x.positions, label = c("Raw mean difference", "N", "Weight"))
+# x.positions <- c(0.25, 0.55, 0.65) + x.max#c(1:3)
+# colLabels <- data.frame(x = x.positions, y = x.positions, label = c("Raw mean difference", "N", "Weight"))
 
 leftPanel <- fp +
   geom_point(aes(size = size, shape = is.summary, color = is.summary)) +
@@ -59,53 +58,55 @@ leftPanel <- fp +
   geom_vline(xintercept = -1, linetype = "dotted") +
   scale_shape_manual(values = c(15,18)) +
   scale_color_manual(values = c('#00CCCC', '#FF8000')) +
-  labs(title = paste0("Summary effect size of ", gene), x = "Raw mean difference") +
+  labs(title = bquote(italic(.(gene))), x = "Raw mean difference") +
   scale_y_discrete(labels = rev(tab$donor)) +
   # scale_x_continuous(limits = c(x.min, x.max)) +
-  geom_text(aes(x = x.max + 0.8, label = rmd), size = 3) +
-  geom_text(aes(x = x.max + 1.8, label = size), size = 3) +
-  geom_text(aes(x = x.max + 2.5, label = weight), size = 3) +
+  geom_text(aes(x = 3.1, label = rmd), size = 3) +
+  geom_text(aes(x = 3.9, label = size), size = 3) +
+  geom_text(aes(x = 4.4, label = weight), size = 3) +
+  geom_text(aes(x = 5, label = benjamini_hochberg), size = 3) +
   # geom_text(data = colLabels, aes(x, y, label = label)) +
   theme.grid + theme(plot.margin = unit(c(0,4,0,4), "lines")) +
   facet.braak
-pdf(file = paste0("forestplot_", gene, ".pdf"), 12, 8)
+pdf(file = paste0("forestplot_", gene, ".pdf"), 12, 3.5)
 leftPanel
 dev.off()
 ##########################################
 
 # Forrest plot Braak correlations
 
-load("resources/summaryCorr.RData")
+load("resources/summaryLabelCorr.RData")
 
 #Get values for a gene to plot
-tab <- summaryCorr[[geneId]]
+tab <- summaryLabelCorr[[geneId]]
 tab$donors <- factor(tab$donors, levels = unique(rev(tab$donors)))
 # Side table info
-tab$rmd <- paste0(round(tab$z, digits = 2), " (", round(tab$lower95, digits = 2), ", ", round(tab$upper95, digits = 2), ")")
+tab$rci <- paste0(round(tab$r, digits = 2), " (", round(tab$lower95, digits = 2), ", ", round(tab$upper95, digits = 2), ")")
 tab$is.summary <- tab$donors == "Summary"
 
 # Forest plot
-fp <- ggplot(data = tab, aes(z, donors)) 
+fp <- ggplot(data = tab, aes(r, donors)) 
 
-x.max <- round(max(tab$upper95), digits = 2)
-x.min <- round(min(tab$lower95), digits = 2)
-x.positions <- c(0.25, 0.55, 0.65) + x.max#c(1:3)
-colLabels <- data.frame(x = x.positions, y = x.positions, label = c("Raw mean difference", "N", "Weight"))
+# x.max <- round(max(tab$upper95), digits = 2)
+# x.min <- round(min(tab$lower95), digits = 2)
+# x.positions <- c(0.25, 0.55, 0.65) + x.max#c(1:3)
+# colLabels <- data.frame(x = x.positions, y = x.positions, label = c("Raw mean difference", "N", "Weight"))
 
 leftPanel <- fp +
   geom_point(aes(size = braakSize, shape = is.summary, color = is.summary)) +
   geom_errorbarh(aes(xmin = lower95, xmax = upper95), height = 0) +
   geom_vline(xintercept = 0, linetype = "dashed") +
   geom_vline(xintercept = 1, linetype = "dotted") +
+  geom_vline(xintercept = -1, linetype = "dotted") +
   # geom_vline(xintercept = -1, linetype = "dotted") +
   scale_shape_manual(values = c(15,18)) +
   scale_color_manual(values = c('#00CCCC', '#FF8000')) +
-  labs(title = paste0("Summary correlation of ", gene), x = "Correlation") +
+  labs(title = bquote(italic(.(gene))), x = "Correlation") +
   scale_y_discrete(labels = rev(tab$donor)) +
   # scale_x_continuous(limits = c(x.min, x.max)) +
-  geom_text(aes(x = x.max + 0.3, label = rmd), size = 3) +
-  geom_text(aes(x = x.max + 0.7, label = braakSize), size = 3) +
-  geom_text(aes(x = x.max + 1.0, label = weight), size = 3) +
+  geom_text(aes(x = 1.6, label = rci), size = 3) +
+  geom_text(aes(x = 2.2, label = braakSize), size = 3) +
+  geom_text(aes(x = 2.6, label = weight), size = 3) +
   # geom_text(data = colLabels, aes(x, y, label = label)) +
   theme.grid + theme(plot.margin = unit(c(0,4,0,4), "lines"))
 pdf(file = paste0("forestplot_corr_", gene, ".pdf"), 8, 2)
