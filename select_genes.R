ef1e1d1b81dbfc62ee7a4a19e6d6ad7994e60555
arlinkeo <arlinkeo@gmail.com>
2018-01-11 15:15:31 +0100

    r >0.6 presence of PD genes

--- select_genes.R
+++ select_genes.R
@@ -8,27 +8,43 @@ source("PD/base_script.R")
 load("resources/summaryDiffExpr.RData")
 load("resources/summaryLabelCorr.RData")
 
-# Select non-Braak reference
+# Select region pair
 diffExpr <- summaryDiffExpr$`braak1-braak6`
 
 #Filter for summary effect
 diffExpr <- do.call(rbind.data.frame, lapply(diffExpr, function(g) g["summary",]))
 labelCor <- do.call(rbind.data.frame, lapply(summaryLabelCorr, function(g) g["summary",]))
 
-# Get correlated genes |r>0.5|
-corrGenes <- rownames(labelCor)[abs(labelCor$r) > 0.5]
+# Get correlated genes |r>0.6|
+corrGenes <- rownames(labelCor)[abs(labelCor$r) > 0.6]
+sum(labelCor[corrGenes, "r"] < 0)
+sum(labelCor[corrGenes, "r"] > 0)
 
 # Diff. expressed genes braak 1 vs. braak 6
-diffGenes <- rownames(labelCor)[diffExpr$benjamini_hochberg < 0.05]
+diffExpr$benjamini_hochberg <- p.adjust(diffExpr$pvalue, method = "BH")
+diffExpr$bonferroni <- p.adjust(diffExpr$pvalue, method = "bonferroni")
+diffGenes <- rownames(diffExpr)[diffExpr$benjamini_hochberg < 0.05 & abs(diffExpr$meanDiff) > 1]
 
 # Correlated and diff. expressed
 braakGenes <- intersect(corrGenes, diffGenes)
+sum(labelCor[braakGenes, "r"] < 0)
+sum(labelCor[braakGenes, "r"] > 0)
 
 # Sort by correlation
 r <- labelCor[braakGenes, "r", drop = FALSE]
-r <- r[order(r),]
+r <- r[order(r), ,drop = FALSE]
 
 save(braakGenes, file = "resources/braakGenes.RData")
-# any(braakGenes== "6622")
 
 #Presence of PD-implicated genes
+pd.genes <- function(x){
+  lapply(pdGenesID, function(l){
+   res <- intersect(l, x)
+   m <- labelCor[res, ]
+   rownames(m) <- entrezId2Name(rownames(m))
+   m
+  })
+}
+
+pd.genes(corrGenes)
+labelCor[corrGenes,]
