49a51de6b3e52a8399ac8a32e9f983cacdee3591
arlinkeo <arlinkeo@gmail.com>
2018-01-11 15:16:48 +0100

    point size p-value calcs

--- volcanoplot_summary_diff.R
+++ volcanoplot_summary_diff.R
@@ -21,6 +21,13 @@ sapply(summaryStats, function(rp){
   sum(rp$benjamini_hochberg < 0.05 & abs(rp$meanDiff) > 1) # significance and 2-fold change
 })
 
+# Correct and transform -values
+summaryStats <- lapply(summaryStats, function(rp){
+  rp$benjamini_hochberg <- p.adjust(rp$pvalue, method = "BH")
+  rp$'logp' <- -log10(rp$benjamini_hochberg)
+  rp
+})
+
 ##### Volcano plot #####
 
 theme <- theme(legend.position = "none",
@@ -29,14 +36,19 @@ theme <- theme(legend.position = "none",
                axis.title =  element_text(size = 12),
                plot.title = element_text(size = 12, face = "bold"))
 
+xmax <- max(sapply(summaryStats, function(rp){max(rp$mean)}))
+xmin <- min(sapply(summaryStats, function(rp){min(rp$mean)}))
+ymax <- max(sapply(summaryStats, function(rp){  max(rp$'logp'[is.finite(rp$'logp')])}))
+
+
 volcano.plot <- function(tab, rp){
-  tab$'logp' <- -log10(tab$benjamini_hochberg)
-  # tab$info <- tab$benjamini_hochberg < 0.05 & abs(tab$meanDiff) > 1
   ggplot(tab, aes(meanDiff, logp, colour = info)) +
-    geom_point(size = 0.5) +
+    geom_point(size = 0.25) +
     scale_colour_manual(values = c("0"="#F8766D", "1"="#00BFC4", "2"="black")) +
     geom_text_repel(label = tab$labels, fontface = "italic", colour = "black", size = 3, nudge_x = 0.2) +
     labs(x = "fold-change", y = "-log10 p-value") +
+    scale_x_continuous(limits = c(xmin, xmax), expand = c(0,0)) +
+    scale_y_continuous(limits = c(0, ymax), expand = c(0,0)) +
     ggtitle(paste("Braak stage ", gsub("braak", "", gsub("-", " vs ", rp)))) +
     theme
 }
@@ -48,6 +60,8 @@ plotll <- lapply(names(summaryStats), function(rp){
   tab$labels[!tab$labels %in% c("SNCA", "GCH1")] <- ""
   tab[name2EntrezId(c("SNCA", "GCH1")), "info"] <- 2
   tab$info <- as.factor(tab$info)
+  order <- order(tab$info)
+  tab <- tab[order, ]
   
   p <- volcano.plot(tab, rp)
   name <- paste0("DiffExpr_braak/volcano_ztest/volcanoplot_", rp, ".pdf")
