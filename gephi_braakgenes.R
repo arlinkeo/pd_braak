e3075830ad92b81d407be9e9cb8df12c6b8af9ad
arlinkeo <arlinkeo@gmail.com>
2018-01-19 00:16:26 +0100

    gephi

--- gephi_braakgenes.R
+++ gephi_braakgenes.R
@@ -1,34 +1,53 @@
 # Gephi plot of Braak-related genes
 
-setwd("C:/Users/dkeo/surfdrive/pd_braak")
+setwd("/tudelft.net/staff-bulk/ewi/insy/DBL/Arlin/pd_braak")
+# setwd("C:/Users/dkeo/surfdrive/pd_braak")
 library("rgexf")
 library(RColorBrewer)
 
 source("PD/base_script.R")
 load("resources/avgCor.RData")
 load("resources/modules.RData")
+load("resources/summaryLabelCorr.RData")
 
-modules <- modules[sample(modules$id, 100), ]
+# modules <- lapply(modules, function(m){ m[sample(m$id, 200), ] })
 
-# Nodes
-genes <- modules$id
-nodes <- data.frame(id = genes, label = entrezId2Name(genes))
-colors <- col2hex(colors()[c(652, 630, 635, 640, 644, 610, 614, 27, 657, 204, 156, 525, 424, 386, 1)])
-names(colors) <- unique(modules$membership)
-memberColor <- sapply(modules$membership, function(x) colors[x])
-nodesAtt <- data.frame(membership = modules$membership, color = memberColor, row.names = rownames(modules))
+# Simplify label correlations to summary estimate only
+labelCor <- sapply(summaryLabelCorr, function(t) unlist(t["summary", "r"]) )
 
-# Edges
-coexpr <- avgCor[genes, genes]
-edges <- as.data.frame(as.table(coexpr))
-colnames(edges) <- c("source", "target", "coexpr")
-edges <- edges[which(edges$coexpr >0.65), ]
-# edges$weight.sign <- sapply(edges$coexpr, function(x) if (x>0) "+" else if (x<0) "-" else "0")
-# edges$coexpr <- abs(edges$coexpr)
-
-write.gexf(nodes = nodes, edges = edges[, c("source", "target")], edgesWeight = edges[, c("coexpr")], 
-           # edgesVizAtt = edgesVizAtt, 
-           # edgesAtt = as.data.frame(edges[, c("weight.sign")]), 
-           # nodesVizAtt = nodeViz, 
-           nodesAtt = nodesAtt,
-           defaultedgetype = "undirected", output = "Images/coexpr_gephi/coexpr.gexf")
+# Write gephi files for pos. and neg. Braak-related genes
+lapply(names(modules), function(r){
+  m <- modules[[r]]
+  
+  # Nodes
+  genes <- m$id
+  nodes <- m[,c("id", "label")]
+  cor <- rep(substr(r, 1, 3), nrow(m))
+  nodeSize <- abs(labelCor[genes]) * 100
+  nodesAtt <- data.frame(membership = m$membership, cor = cor)
+  nodeCol <- as.data.frame(t(col2rgb(m$color)))
+  nodeCol$alpha <- rep(1, nrow(nodeCol))
+  nodesVizAtt <- list(color = nodeCol, size = nodeSize)
+  
+  # Edges
+  coexpr <- avgCor[genes, genes]
+  # lower.tri(coexpr) <- 0 # rm duplicate links
+  diag(coexpr) <- 0
+  edges <- as.data.frame(as.table(coexpr))
+  colnames(edges) <- c("source", "target", "coexpr")
+  edges <- edges[edges$coexpr > 0.5, ] # filter edges 
+  # print(min(edges$coexpr))
+  colPal <- rev(heat.colors(100))
+  edgeCol <- colPal[as.numeric(cut(edges$coexpr, breaks = seq(0,1, by =0.01)))]
+  edgeCol <- as.data.frame(t(col2rgb(edgeCol)))
+  edgeCol$alpha <- rep(1, nrow(edgeCol))
+  edgesVizAtt <- list(color = edgeCol)
+  
+  
+  write.gexf(nodes = nodes, edges = edges[, c("source", "target")], edgesWeight = edges$coexpr, 
+             edgesVizAtt = edgesVizAtt,
+             # edgesAtt = edgesAtt,
+             nodesVizAtt = nodesVizAtt,
+             nodesAtt = nodesAtt,
+             defaultedgetype = "undirected", output = paste0("coexpr_", r, ".gexf"))
+})
