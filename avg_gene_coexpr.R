cc97b8b0fea99d223c67dcf78c61f3738263e8ce
arlinkeo <arlinkeo@gmail.com>
2018-02-01 00:38:58 +0100

    in gene_coexpr.R

--- avg_gene_coexpr.R
+++
@@ -1,9 +0,0 @@
-# Average co-expression
-setwd("/tudelft.net/staff-bulk/ewi/insy/DBL/Arlin/pd_braak")
-
-source("PD/base_script.R")
-load("resources/gene_coexpr.Rdata") # list with corr matries for each donor
-
-gene_coexpr <- lapply(gene_coexpr, function(x)x$r)
-avgCor <- apply(simplify2array(gene_coexpr), 1:2, mean)
-save(avgCor, file = "resources/avgCor.RData")
