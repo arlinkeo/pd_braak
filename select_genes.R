147cb7913cf971b8158092e310571b5a40572ff2
arlinkeo <arlinkeo@gmail.com>
2018-02-01 22:59:34 +0100

    thresholds print table

--- select_genes.R
+++ select_genes.R
@@ -16,14 +16,14 @@ diffExpr <- do.call(rbind.data.frame, lapply(diffExpr, function(g) g["summary",]
 labelCor <- do.call(rbind.data.frame, lapply(summaryLabelCorr, function(g) g["summary",]))
 
 # Get correlated genes |r>0.6|
-corrGenes <- rownames(labelCor)[abs(labelCor$r) > 0.6]
+corrGenes <- rownames(labelCor)[abs(labelCor$r) > 0.65]
 sum(labelCor[corrGenes, "r"] < 0)
 sum(labelCor[corrGenes, "r"] > 0)
 
 # Diff. expressed genes braak 1 vs. braak 6
 diffExpr$benjamini_hochberg <- p.adjust(diffExpr$pvalue, method = "BH")
-# diffExpr$bonferroni <- p.adjust(diffExpr$pvalue, method = "bonferroni")
-diffGenes <- rownames(diffExpr)[diffExpr$benjamini_hochberg < 0.001 & abs(diffExpr$meanDiff) > 1]
+diffExpr$bonferroni <- p.adjust(diffExpr$pvalue, method = "bonferroni")
+diffGenes <- rownames(diffExpr)[diffExpr$benjamini_hochberg < 0.00125 & abs(diffExpr$meanDiff) > 1.5]
 
 # Correlated and diff. expressed
 braakGenes <- intersect(corrGenes, diffGenes)
@@ -44,11 +44,24 @@ save(braakGenes, file = "resources/braakGenes.RData")
 #Presence of PD-implicated genes
 pd.genes <- function(x){
   lapply(pdGenesID, function(l){
-   res <- intersect(l, x)
-   m <- labelCor[res, ]
-   rownames(m) <- entrezId2Name(rownames(m))
-   m
+   intersect(l, x)
   })
 }
 
-pd.genes(unlist(braakGenes))
+pd <- pd.genes(unlist(braakGenes))
+
+# Print braak genes with correlations, mean diff, and p-values
+gene.stats <- function(g){
+  r <- labelCor[g, "r", drop = FALSE] 
+  geneorder <- rownames(r)[order(r)]
+  r <- r[geneorder, ]
+  de <- diffExpr[geneorder, c("meanDiff", "benjamini_hochberg")]
+  id <- geneorder
+  name <- entrezId2Name(geneorder)
+  df <- data.frame(id, name, r, de)
+  df
+}
+
+tab <- Reduce(rbind, lapply(pd , gene.stats))
+
+write.table(tab, file = "pdgenes_stats.txt", sep ="\t", quote = FALSE, row.names = FALSE)
