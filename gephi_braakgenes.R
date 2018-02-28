d0069517f239ecf4addf1506745e374085ba6d8a
arlinkeo <arlinkeo@gmail.com>
2018-02-28 18:41:59 +0100

    node info

--- gephi_braakgenes.R
+++ gephi_braakgenes.R
@@ -1,47 +1,61 @@
-# Gephi plot of Braak-related genes
+# Gephi plot of modules with Braak-related genes
 
-setwd("/tudelft.net/staff-bulk/ewi/insy/DBL/Arlin/pd_braak")
-# setwd("C:/Users/dkeo/surfdrive/pd_braak")
-library("rgexf")
-library(RColorBrewer)
-
+# setwd("/tudelft.net/staff-bulk/ewi/insy/DBL/Arlin/pd_braak")
 source("PD/base_script.R")
-load("resources/avgCor.RData")
-load("resources/modules.RData")
+setwd("C:/Users/dkeo/surfdrive/pd_braak")
+library("rgexf")
+
+load("resources/braakGenes.RData")
+braakGenes <- unlist(braakGenes)
+
+load("resources/modules_braak.RData")
+modules_braak <- lapply(modules_braak, unlist)
+# modules_braak <- lapply(modules_braak, function(m){ sample(m, 200) })
+
+load("resources/hierclust_tree.RData")
+
+# Load co-expression matrices
+braakCoexpr <- list(
+  'braak1' = readRDS("resources/avgCoexpr_braak1.rds"), 
+  'braak6' = readRDS("resources/avgCoexpr_braak6.rds")
+)
+
+# Summary label correlations
 load("resources/summaryLabelCorr.RData")
-
-# modules <- lapply(modules, function(m){ m[sample(m$id, 200), ] })
-
-# Simplify label correlations to summary estimate only
 labelCor <- sapply(summaryLabelCorr, function(t) unlist(t["summary", "r"]) )
 
-# Write gephi files for pos. and neg. Braak-related genes
-lapply(names(modules), function(r){
-  m <- modules[[r]]
+# Write gephi files for modules in Braak 1 and 6
+lapply(names(modules_braak), function(r){
+  m <- hierclust_tree[[r]][["average"]]
+  genes <- modules_braak[[r]]
+  gene_idx <- match(genes, m$labels)
   
   # Nodes
-  genes <- m$id
-  nodes <- m[,c("id", "label")]
-  cor <- rep(substr(r, 1, 3), nrow(m))
-  nodeSize <- abs(labelCor[genes]) * 100
-  nodesAtt <- data.frame(membership = m$membership, cor = cor)
-  nodeCol <- as.data.frame(t(col2rgb(m$color)))
+  nodes <- data.frame(id = genes, label = entrezId2Name(genes))
+  cor <- labelCor[gene_idx]
+  # nodeSize <- ifelse(genes %in% braakGenes, 2, 1)#abs(cor) * 100
+  braak_gene <- ifelse(genes %in% braakGenes, 1, 0)
+  nodesAtt <- data.frame(membership = m$module[gene_idx], cor = cor, braak_gene = braak_gene)
+  nodeCol <- data.frame(t(col2rgb(m$color[gene_idx])))
   nodeCol$alpha <- rep(1, nrow(nodeCol))
-  nodesVizAtt <- list(color = nodeCol, size = nodeSize)
+  nodesVizAtt <- list(color = nodeCol)#, size = nodeSize)
   
   # Edges
-  coexpr <- avgCor[genes, genes]
-  # lower.tri(coexpr) <- 0 # rm duplicate links
+  coexpr <- braakCoexpr[[r]]
+  coexpr <- coexpr[genes, genes]
+  coexpr[upper.tri(coexpr)] <- 0 # rm duplicate links
   diag(coexpr) <- 0
   edges <- as.data.frame(as.table(coexpr))
   colnames(edges) <- c("source", "target", "coexpr")
-  edges <- edges[edges$coexpr > 0.5, ] # filter edges 
+  edges <- edges[edges$coexpr != 0, ] # filter edges 
+  edges <- edges[abs(edges$coexpr) > 0.5, ] # 
+  filter edges 
   # print(min(edges$coexpr))
-  colPal <- rev(heat.colors(100))
-  edgeCol <- colPal[as.numeric(cut(edges$coexpr, breaks = seq(0,1, by =0.01)))]
+  edgeCol <- c("blue", "red")[as.numeric(edges$coexpr>0)+1]
   edgeCol <- as.data.frame(t(col2rgb(edgeCol)))
   edgeCol$alpha <- rep(1, nrow(edgeCol))
   edgesVizAtt <- list(color = edgeCol)
+  edges$coexpr <- abs(edges$coexpr)
   
   
   write.gexf(nodes = nodes, edges = edges[, c("source", "target")], edgesWeight = edges$coexpr, 
