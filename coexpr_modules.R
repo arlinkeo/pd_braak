7b4bd36c3595af8ddf364f1b21286c41f83f8267
arlinkeo <arlinkeo@gmail.com>
2018-03-07 21:09:54 +0100

    save memory

--- coexpr_modules.R
+++ coexpr_modules.R
@@ -4,34 +4,30 @@ setwd("C:/Users/dkeo/surfdrive/pd_braak")
 source("PD/base_script.R")
 library(WGCNA)
 
-# Load co-expression matrices
-braakCoexpr <- list(
-  'braak1' = readRDS("resources/avgCoexpr_braak1.rds"), 
-  'braak6' = readRDS("resources/avgCoexpr_braak6.rds")
-)
-
 ### Hierarchical clustering and module detection ###
-hierclust_tree <- sapply(braakCoexpr, function(coexpr){
-  # Distance matrix
+regions <- c("braak1", "braak6", "braak1-6")
+hierclust_tree <- sapply(regions, function(r){
+  file <- paste0("resources/avgCoexpr_", r, ".rds")
+  coexpr <- readRDS(file)
   dist <- as.dist(1 - abs(coexpr))
   sapply(c("single", "complete", "average"), function(method){
     # Hierarchical clustering Braak 1
     tree <- hclust(dist, method = method)
     # Co-expression modules per Braak region
-    tree$module <- cutreeDynamicTree(tree)
+    tree$module <- cutreeDynamicTree(tree, minModuleSize = 50)
     table(tree$module) # count
     tree$color <- labels2colors(tree$module)
     tree;
   }, simplify = FALSE)
 }, simplify = FALSE)
-save(hierclust_tree, file = "resources/hierclust_tree.RData")
+save(hierclust_tree, file = "resources/hierclust_tree_absCor.RData")
 
 # Plot dendrogram and modules 
-pdf("coexpr_modules.pdf", 12, 2)
+pdf("coexpr_modules_absCor.pdf", 12, 2)
 lapply(names(hierclust_tree), function(b) {
   treeList <- hierclust_tree[[b]]
   lapply(names(treeList), function(method){
-    title <- "" # paste0("Co-expression modules in Braak ", b, ", ", method, " linkage")
+    title <- paste0("Co-expression modules in Braak ", b, ", ", method, " linkage")
     tree <- treeList[[method]]
     plotDendroAndColors(tree, colors = tree$color, dendroLabels = FALSE, marAll = c(0, 4, 0.2, 0), main = title)
   })
@@ -54,4 +50,4 @@ modules <- lapply(hierclust_tree, function(b){
     tree$labels[tree$module == m]
   })
 })
-save(modules, file = "resources/modules.RData")
+save(modules, file = "resources/modules_abscor.RData")
