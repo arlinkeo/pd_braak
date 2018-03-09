ebdaddb1b5dd7c01890b838118b8382803f0264f
arlinkeo <arlinkeo@gmail.com>
2018-03-09 15:07:50 +0100

    top 10% selection

--- volcanoplot_summary_diff.R
+++ volcanoplot_summary_diff.R
@@ -7,25 +7,26 @@ library(gridExtra)
 library(ggrepel)
 
 source("PD/base_script.R")
-load("resources/summaryDiffExpr_ztest.RData")
+load("resources/summaryDiffExpr.RData")
 
-# Extract summary statistics
-summaryStats <- lapply(summaryDiffExpr, function(rp){
-  as.data.frame(t(sapply(rp, function(g) {
-    unlist(g["summary", !colnames(g) %in% c("donors", "weight")])
-  })))
-})
-
-# Number of diff. expr. genes for each region pair
-sapply(summaryStats, function(rp){
-  sum(rp$benjamini_hochberg < 0.05 & abs(rp$meanDiff) > 1) # significance and 2-fold change
-})
-
-# Correct and transform p-values
-summaryStats <- lapply(summaryStats, function(rp){
+# Extract summary statistics, correct p-values
+diffExpr <- lapply(summaryDiffExpr, function(rp){
+  # as.data.frame(t(sapply(rp, function(g) {
+  #   unlist(g["summary", !colnames(g) %in% c("donors", "weight")])
+  # })))
+  rp <- do.call(rbind.data.frame, lapply(rp, function(g) g["summary",]))
   rp$benjamini_hochberg <- p.adjust(rp$pvalue, method = "BH")
   rp$'logp' <- -log10(rp$benjamini_hochberg)
   rp
+})
+
+# Number of diff. expr. genes for each region pair after correction
+diffGenes <- sapply(diffExpr, function(rp){
+  order <- order(rp$benjamini_hochberg) # order absolute corr.
+  diffGenes1 <- rownames(rp)[order[1:1992]] # top 10% genes
+  order <- rev(order(abs(rp$meanDiff))) # order absolute corr.
+  diffGenes2 <- rownames(rp)[order[1:1992]] # top 10% genes
+  intersect(diffGenes1, diffGenes2)
 })
 
 ##### Volcano plot #####
@@ -36,9 +37,9 @@ theme <- theme(legend.position = "none",
                axis.title =  element_text(size = 12),
                plot.title = element_text(size = 12, face = "bold"))
 
-xmax <- max(sapply(summaryStats, function(rp){max(rp$mean)}))
-xmin <- min(sapply(summaryStats, function(rp){min(rp$mean)}))
-ymax <- max(sapply(summaryStats, function(rp){  max(rp$'logp'[is.finite(rp$'logp')])}))
+xmax <- max(sapply(diffExpr, function(rp){max(rp$mean)}))
+xmin <- min(sapply(diffExpr, function(rp){min(rp$mean)}))
+ymax <- max(sapply(diffExpr, function(rp){  max(rp$'logp'[is.finite(rp$'logp')])}))
 
 
 volcano.plot <- function(tab, rp){
@@ -53,18 +54,20 @@ volcano.plot <- function(tab, rp){
     theme
 }
 
-plotll <- lapply(names(summaryStats), function(rp){
-  tab <- summaryStats[[rp]]
-  tab$info <- as.numeric(tab$benjamini_hochberg < 0.05 & abs(tab$meanDiff) > 1)
+plotll <- lapply(names(diffExpr), function(rp){
+  tab <- diffExpr[[rp]]
+  deg <- diffGenes[[rp]]
+  tab$info <- as.numeric(rownames(tab) %in% deg)
+  # tab$info <- as.numeric(tab$benjamini_hochberg < 0.05 & abs(tab$meanDiff) > 1.54)
   tab$labels <- entrezId2Name(rownames(tab))
-  tab$labels[!tab$labels %in% c("SNCA", "GCH1")] <- ""
-  tab[name2EntrezId(c("SNCA", "GCH1")), "info"] <- 2
+  tab$labels[!tab$labels %in% c("SNCA")] <- ""
+  tab[name2EntrezId(c("SNCA")), "info"] <- 2
   tab$info <- as.factor(tab$info)
   order <- order(tab$info)
   tab <- tab[order, ]
   
   p <- volcano.plot(tab, rp)
-  name <- paste0("DiffExpr_braak/volcano_ztest/volcanoplot_", rp, ".pdf")
+  name <- paste0("DiffExpr_braak/volcano_ttest/volcanoplot_", rp, ".pdf")
   pdf(file = name, 4, 3)
   print(p)
   dev.off()
