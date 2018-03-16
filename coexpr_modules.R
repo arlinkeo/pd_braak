778f0dae9b5eeb2bfe5bb1d396a983d5e2c66041
arlinkeo <arlinkeo@gmail.com>
2018-03-16 13:32:17 +0100

    non abs cor

--- coexpr_modules.R
+++ coexpr_modules.R
@@ -9,21 +9,21 @@ regions <- c("braak1", "braak6", "braak1-6")
 hierclust_tree <- sapply(regions, function(r){
   file <- paste0("resources/avgCoexpr_", r, ".rds")
   coexpr <- readRDS(file)
-  dist <- as.dist(1 - abs(coexpr))
+  dist <- as.dist(1 - coexpr)
   sapply(c("single", "complete", "average"), function(method){
     # Hierarchical clustering Braak 1
     tree <- hclust(dist, method = method)
     # Co-expression modules per Braak region
-    tree$module <- cutreeDynamicTree(tree, minModuleSize = 50)
+    tree$module <- cutreeDynamicTree(tree)
     table(tree$module) # count
     tree$color <- labels2colors(tree$module)
     tree;
   }, simplify = FALSE)
 }, simplify = FALSE)
-save(hierclust_tree, file = "resources/hierclust_tree_absCor.RData")
+save(hierclust_tree, file = "resources/hierclust_tree.RData")
 
 # Plot dendrogram and modules 
-pdf("coexpr_modules_absCor.pdf", 12, 2)
+pdf("coexpr_modules.pdf", 12, 2)
 lapply(names(hierclust_tree), function(b) {
   treeList <- hierclust_tree[[b]]
   lapply(names(treeList), function(method){
@@ -50,4 +50,21 @@ modules <- lapply(hierclust_tree, function(b){
     tree$labels[tree$module == m]
   })
 })
-save(modules, file = "resources/modules_abscor.RData")
+save(modules, file = "resources/modules.RData")
+
+# overlap of genes between modules in B1 and B6
+overlap <- sapply(modules$braak1, function(m1){
+  sapply(modules$braak6, function(m6){
+    overlap <- length(intersect(m1, m6))
+    jaccard <- overlap/(length(m1) + length(m6) - overlap)
+    round(jaccard*100, digits = 0)
+  })
+})
+library(gplots)
+rampcols <- colorRampPalette(colors = c("blue", "white", "red"), space="Lab")(200)
+heatmap.2(overlap, col = rampcols, 
+          Rowv=FALSE, Colv=FALSE, 
+          cexCol = 1, cexRow = 1,
+          scale = "none", trace = "none", dendrogram = "none", 
+          main = "Jaccard index module overlap")
+#, lwid = c(1, 10), lhei = c(1,10))
