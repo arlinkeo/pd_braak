45eb126a6ad8927bf89303ac4f9209c71158e4d4
arlinkeo <arlinkeo@gmail.com>
2018-03-16 19:29:02 +0100

    print table braak1-6 only

--- module_braakgene_overlap.R
+++ module_braakgene_overlap.R
@@ -3,43 +3,48 @@ setwd("C:/Users/dkeo/surfdrive/pd_braak")
 source("PD/base_script.R")
 load("resources/modules.RData")
 load("resources/braakGenes.RData")
+load("resources/summaryLabelCorrEG.RData")
 
 braakGenes <- unlist(braakGenes)
+labelCor <- do.call(rbind.data.frame, lapply(summaryLabelCorrEG, function(g) g["summary",]))
+total <- length(ahba.genes())
+orderEG <- rev(order(labelCor$r))
 
-total = 19992
-module_overlap <- lapply(modules, function(m){
-  tab <- as.data.frame(t(sapply(m, function(module){
-    genes <- intersect(module, braakGenes)
-    overlap <- length(genes)
-    ns1 <- length(module)
-    ns2 <- length(braakGenes)
-    p <- phyper(overlap - 1, ns1, total - ns1, ns2, lower.tail = FALSE)
-    c(overlap = overlap, module.size = ns1, pvalue = p)
-  })))
-  tab$benjamini_hochberg <- p.adjust(tab$pvalue)
-  tab <- tab[tab$benjamini_hochberg < 0.05, ]
-  tab[order(tab$benjamini_hochberg), ]
-})
+b= "braak1-6"
+m <- modules[[b]]
+tab <- as.data.frame(t(sapply(m, function(module){ # for each module
+  genes <- intersect(module, braakGenes)
+  overlap <- length(genes)
+  ns1 <- length(module)
+  ns2 <- length(braakGenes)
+  p <- phyper(overlap - 1, ns1, total - ns1, ns2, lower.tail = FALSE)
+  c(overlap = overlap, module.size = ns1, pvalue = p)
+})))
+tab$benjamini_hochberg <- p.adjust(tab$pvalue)
+# tab <- tab[tab$benjamini_hochberg < 0.05, ] # select only significant modules
+# tab[order(tab$benjamini_hochberg), ]
+tab$r_braak <- labelCor$r
 
-lapply(names(module_overlap), function(b){
-  tab <-  module_overlap[[b]]
-  tab$module <- rownames(tab)
-  write.table(tab, file = paste0("module_braakgene_overlap_", b, ".txt"), sep ="\t", quote = FALSE, row.names = FALSE)
-})
-modules_braak <- sapply(c("braak1", "braak6", "braak1-6"), function(b){
-  modNames <- rownames(module_overlap[[b]])
-  modules[[b]][modNames]
-}, simplify = FALSE)
-save(modules_braak, file = "resources/modules_braak.RData")
+# modules_braak <- sapply(c("braak1", "braak6", "braak1-6"), function(b){
+#   modNames <- rownames(module_overlap[[b]])
+#   modules[[b]][modNames]
+# }, simplify = FALSE)
+# save(modules_braak, file = "resources/modules_braak.RData")
 
-sapply(modules_braak, function(m){
-  present <- sapply(names(m), function(n){
-    module <- m[[n]]
-    present <- sapply(pdGenes$lysosome, function(g){
-      any(module == name2EntrezId(g))
-    })
-    present <- present[!is.na(present)]
-    paste0(names(present)[present], collapse = ",")
+# Add enrichment of PD genes
+pdGenesID$progression <- braakGenes
+pdOverlap <-  sapply(pdGenesID, function(pd){
+  sapply(m, function(genes){
+    res <- intersect(genes, pd)
+    paste0(entrezId2Name(res), collapse = ",")
   })
-  
 })
+
+tab <- cbind(tab, pdOverlap)
+tab <- tab[orderEG, ]
+
+
+# Print results in text-file
+tab$pvalue <- NULL
+tab <- cbind(module = rownames(tab), tab)
+write.table(tab, file = paste0("module_braakgene_overlap_", b, ".txt"), sep ="\t", quote = FALSE, row.names = FALSE)
