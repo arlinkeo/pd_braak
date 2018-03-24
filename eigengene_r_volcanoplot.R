7c0dba722b7f225fdf104cd3c4fdd2bc712880c9
arlinkeo <arlinkeo@gmail.com>
2018-03-24 22:57:24 +0100

    clean

--- eigengene_r_volcanoplot.R
+++ eigengene_r_volcanoplot.R
@@ -5,7 +5,7 @@ source("PD/base_script.R")
 library(ggplot2)
 library(gridExtra)
 library(ggrepel)
-
+load("resources/module_enrichment.RData")
 load("resources/summaryLabelCorrEG.RData")
 labelCor <- do.call(rbind.data.frame, lapply(summaryLabelCorrEG, function(g) g["summary",]))
 
@@ -22,37 +22,33 @@ tab$'logp' <- -log10(tab$benjamini_hochberg)
 corrEG <- rownames(labelCor)[tab$benjamini_hochberg < 0.001] # significant correlated modules
 tab$info <- as.numeric(rownames(tab) %in% corrEG)
 
-braakMod <- read.table(file = "module_braakgene_overlap_braak1-6.txt", sep = "\t", header = TRUE)
-braakMod <- braakMod$module[braakMod$benjamini_hochberg < 0.05] # significant overlap with braak genes
-tab$labels <- rownames(tab)
+braakMod <- module_enrichment$module[module_enrichment$pvalue_progression < 0.001]
+tab$labels <- module_enrichment$module
 tab$labels[!tab$labels %in% braakMod] <- ""
-tab[braakMod, "info"] <- 2
+tab[tab$labels %in% braakMod, "info"] <- 2
 
 # Plotting order of data points 
 tab$info <- as.factor(tab$info)
 order <- order(tab$info)
 tab <- tab[order, ]
 
-xmax <- max(tab$r)
-xmin <- min(tab$r)
-ymax <-  max(tab$'logp'[is.finite(tab$'logp')])
+xmax <- max(tab$r)+.2
+xmin <- min(tab$r)-.2
+ymax <-  ceiling(max(tab$'logp'))
 p <- ggplot(tab, aes(r, logp, colour = info)) +
-  geom_point(size = 0.25) +
+  geom_point(size = 2) +
   scale_colour_manual(values = c("0"="#F8766D", "1"="#00BFC4", "2"="black")) +
-  geom_text_repel(label = tab$labels, colour = "black", size = 3, nudge_x = 0.2) +
+  geom_text_repel(label = tab$labels, colour = "black", size = 3, nudge_x = 0) +
   labs(x = "Pearson's r", y = "-log10 p-value") +
   scale_x_continuous(limits = c(xmin, xmax), expand = c(0,0)) +
   scale_y_continuous(limits = c(0, ymax), expand = c(0,0)) +
   ggtitle("Eigen gene-Braak label correlation") +
+  annotate("label", label ="Downregulated", x=0.7, y=0.5) +
+  annotate("label", label ="Upregulated", x=-0.7, y=0.5) +
   theme
-
-order <- order(tab$r)
-label <- rownames(tab)[order]
-color <- ifelse(label %in% corrEG, "#F8766D", "#00BFC4")
-barplot(tab$r[order], horiz = TRUE, col = color)
-hist(tab$r, breaks = 30)
+p
 
 name <- paste0("eigengene_r_volcanoplot.pdf")
-pdf(file = name, 4, 3)
+pdf(file = name, 6, 4.5)
 print(p)
 dev.off()
