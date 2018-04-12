a6b804c7a27d92dd300fc12a822a6b8d1221aa14
arlinkeo <arlinkeo@gmail.com>
2018-04-12 17:47:08 +0200

    plot options

--- volcanoplot_summary_diff.R
+++ volcanoplot_summary_diff.R
@@ -30,33 +30,39 @@ theme <- theme(legend.position = "none",
                panel.background = element_blank(),
                axis.line = element_line(colour = "black"),
                axis.title =  element_text(size = 12),
-               plot.title = element_text(size = 12, face = "bold")) +
-  
-  
+               plot.title = element_text(size = 12, face = "bold"))
+
+# corrected and log-transformed p-values
+diffExpr <- lapply(diffExpr, function(tab){
+  tab$benjamini_hochberg <- p.adjust(tab$pvalue, method = "BH")
+  tab$'logp' <- -log10(tab$benjamini_hochberg)
+  tab
+})
+
+# axis limits for all plots
+ctab <- Reduce(rbind, diffExpr)
+xmax <- max(ctab$meanDiff)
+xmin <- min(ctab$meanDiff)
+ymax <- ceiling(max(ctab$'logp'[is.finite(tab$'logp')]))
 
 plotll <- lapply(names(diffExpr), function(rp){
   tab <- diffExpr[[rp]]
-  tab$benjamini_hochberg <- p.adjust(tab$pvalue, method = "BH")
-  tab$'logp' <- -log10(tab$benjamini_hochberg)
   tab$info <- as.numeric(rownames(tab) %in% braakGenes)
   
-  tab$labels <- entrezId2Name(rownames(tab))
-  pd <- c("DNAJC13","SNCA","GCH1","INPP5F", "ASH1L", "SPACA3", "NPC2", "IFI30", "TSPAN8", "CORO1A")
-  tab$labels[!tab$labels %in% pd] <- ""
-  tab[name2EntrezId(pd), "info"] <- 2
+  # tab$labels <- entrezId2Name(rownames(tab))
+  # pd <- c("DNAJC13","SNCA","GCH1","INPP5F", "ASH1L", "ITPKB", "ELOVL7", "ZNF184", "SCARB2", "DDRGK1")
+  # tab$labels[!tab$labels %in% pd] <- ""
+  # tab[name2EntrezId(pd), "info"] <- 2
   
   # Plotting order of data points 
   tab$info <- as.factor(tab$info)
   order <- order(tab$info)
   tab <- tab[order, ]
-  
-  xmax <- max(tab$meanDiff)
-  xmin <- min(tab$meanDiff)
-  ymax <-  max(tab$'logp'[is.finite(tab$'logp')])
-  ggplot(tab, aes(meanDiff, logp, colour = info)) +
-    geom_point(size = 0.25) +
+
+  p <- ggplot(tab, aes(meanDiff, logp, colour = info)) +
+    geom_point(size = 0.25, alpha = 0.3) +
     scale_colour_manual(values = c("0"="#F8766D", "1"="#00BFC4", "2"="black")) +
-    geom_text_repel(label = tab$labels, fontface = "italic", colour = "black", size = 3, nudge_x = 0.2) +
+    # geom_text_repel(label = tab$labels, fontface = "italic", colour = "black", size = 3, nudge_x = 0.2) +
     labs(x = "fold-change", y = "-log10 p-value") +
     scale_x_continuous(limits = c(xmin, xmax), expand = c(0,0)) +
     scale_y_continuous(limits = c(0, ymax), expand = c(0,0)) +
