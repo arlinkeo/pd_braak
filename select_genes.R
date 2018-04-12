ea03f503d1fd57a19b0d26ad8fffdc945f1aa52c
arlinkeo <arlinkeo@gmail.com>
2018-04-12 17:45:33 +0200

    top 10% correct number

--- select_genes.R
+++ select_genes.R
@@ -13,27 +13,32 @@ diffExpr <- summaryDiffExpr$`braak1-braak6`
 diffExpr <- do.call(rbind.data.frame, lapply(diffExpr, function(g) g["summary",]))
 labelCor <- do.call(rbind.data.frame, lapply(summaryLabelCorr, function(g) g["summary",]))
 
+# number of genes in top 10
+top10 <- floor(nrow(labelCor)*0.1)
+
 # Get top 10% correlated genes
 order <- rev(order(abs(labelCor$r))) # order absolute corr.
-corrGenes <- rownames(labelCor)[order[1:1992]] # top 10% genes
-sum(labelCor[corrGenes, "r"] < 0)
-sum(labelCor[corrGenes, "r"] > 0)
+corrGenes <- rownames(labelCor)[order[1:top10]] # top 10% genes
+corrGenes_neg <- corrGenes[labelCor[corrGenes, "r"] < 0]
+corrGenes_pos <- corrGenes[labelCor[corrGenes, "r"] > 0]
+min(abs(labelCor[corrGenes, "r"]))
 
 # Top 10% significant Diff. expressed genes braak 1 vs. braak 6
 diffExpr$benjamini_hochberg <- p.adjust(diffExpr$pvalue, method = "BH")
 diffExpr$bonferroni <- p.adjust(diffExpr$pvalue, method = "bonferroni")
 order <- order(diffExpr$benjamini_hochberg) # order absolute corr.
-diffGenes1 <- rownames(diffExpr)[order[1:1992]] # top 10% genes
-sum(diffExpr[diffGenes1, "meanDiff"] < 0)
-sum(diffExpr[diffGenes1, "meanDiff"] > 0)
+diffGenes1 <- rownames(diffExpr)[order[1:top10]] # top 10% genes
+diffGenes1_up <- diffGenes1[diffExpr[diffGenes1, "meanDiff"] > 0]
+diffGenes1_down <- diffGenes1[diffExpr[diffGenes1, "meanDiff"] < 0]
+max(diffExpr[diffGenes1, "benjamini_hochberg"])
 
 # Top 10% mean Diff. expressed genes braak 1 vs. braak 6
 order <- rev(order(abs(diffExpr$meanDiff))) # order absolute corr.
-diffGenes2 <- rownames(diffExpr)[order[1:1992]] # top 10% genes
-sum(diffExpr[diffGenes2, "meanDiff"] < 0)
-sum(diffExpr[diffGenes2, "meanDiff"] > 0)
+diffGenes2 <- rownames(diffExpr)[order[1:top10]] # top 10% genes
+diffGenes2_up <- diffGenes2[diffExpr[diffGenes2, "meanDiff"] > 0]
+diffGenes2_down <- diffGenes2[diffExpr[diffGenes2, "meanDiff"] < 0]
+min(abs(diffExpr[diffGenes2, "meanDiff"]))
 
-# Correlated and diff. expressed
 braakGenes <- Reduce(intersect, list(corrGenes, diffGenes1, diffGenes2))
 positive_r <- braakGenes[labelCor[braakGenes, "r"] > 0]
 negative_r <- braakGenes[labelCor[braakGenes, "r"] < 0]
