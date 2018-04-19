4ba2ee9805eccedfbbd0142b489b0348301f40f3
arlinkeo <arlinkeo@gmail.com>
2018-04-19 22:39:03 +0200

    protein coding genes only multiple testing correction for the number of progression genes

--- diff_expr_GTEX.R
+++ diff_expr_GTEX.R
@@ -4,33 +4,51 @@ setwd("C:/Users/dkeo/surfdrive/pd_braak")
 source("PD/base_script.R")
 
 df <- read.table("../GTEX/GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_tpm.gct", sep = "\t", comment.char = "#", header = TRUE, skip = 2)
-rownames(df) <- df$Name
-# load("../GTEX/gtex_expr_norm.RData")
-# rownames(gtex_expr_norm) <- gtex_expr_norm$Name
+# rownames(df) <- df$Name
 
-annot <- read.table("../GTEX/GTEx_v7_Annotations_SampleAttributesDS.txt", sep = "\t", header = TRUE, fill = T, quote = "")
+# Filter IDs for protein coding genes only
+gene.annot <- read.table("../GTEX/gencode.v19.genes.v7.patched_contigs.gtf", sep = "\t")
+gene.annot <- gene.annot[which(gene.annot$V3 == "gene"), ]
+gene_info <- strsplit(gene.annot$V9, split = ";")
+gene_type <- sapply(gene_info,  function(x){
+  gt <- x[[grep("gene_type", x)]]
+  gsub(" gene_type ", "", gt)
+})
+gene_id <- sapply(gene_info,  function(x){
+  id <- x[[grep("gene_id", x)]]
+  gsub("gene_id ", "", id)
+})
+genes <- gene_id[gene_type == "protein_coding"]
+
+df <- df[genes, ]
+genes <- sapply(genes, function(s) {# remove version from ID
+  unlist(strsplit(s, split = "\\."))[1]
+})
+names(genes) <- genes
+rownames(df) <- genes
+
+# Select samples corresponding to brain tissues of Braak regions
+sample.annot <- read.table("../GTEX/GTEx_v7_Annotations_SampleAttributesDS.txt", sep = "\t", header = TRUE, fill = T, quote = "")
 
 roi <- c("Substantia nigra", "Amygdala", "Anterior cingulate cortex", "Frontal Cortex")
 names(roi) <- c("SN", "AMG", "ACC", "FC")
 samples <- lapply(roi, function(r){
-  rows <- grep(r, annot$SMTSD)
-  ids <- gsub("-", ".", annot[rows, "SAMPID"])
-  intersect(ids, colnames(gtex_expr_norm))
+  rows <- grep(r, sample.annot$SMTSD)
+  ids <- gsub("-", ".", sample.annot[rows, "SAMPID"])
+  intersect(ids, colnames(df))
 })
 
-tab <- lapply(samples, function(s){
-  gtex_expr_norm[, s]
+# select samples for each region
+dfList <- lapply(samples, function(s){ 
+  df[, s]
 })
-
-# T-test
-genes <-gtex_expr_norm$Name
 
 # T-test function for 2 regions
 ttest.regions <- function(a,b){
   as.data.frame(t(sapply(genes, function(g){
     print(paste0(a, "-", b, "; ", which(genes %in% g), "; ", g))
-    r1 <- unlist(tab[[a]][g, ])
-    r2 <- unlist(tab[[b]][g, ])
+    r1 <- unlist(dfList[[a]][g, ])
+    r2 <- unlist(dfList[[b]][g, ])
     t <- t.test(r1, r2)
     meanDiff <- t$estimate[1]-t$estimate[2]
     pvalue <- t$p.value
@@ -38,19 +56,15 @@ ttest.regions <- function(a,b){
   })))
 }
 
+# T-test
 regionpairs <- combn(names(roi), 2)
 colnames(regionpairs) <- apply(regionpairs, 2, function(x) paste0(x[1], "-",  x[2]))
 ttest.all <- apply(regionpairs, 2, function(x){
   r1 <- x[1]
   r2 <- x[2]
-  ttest <- ttest.regions(r1, r2)
-  file <- paste0("Resources/ttest_gtex_", r1, "_", r2, ".RData")
-  print(file)
-  save(ttest, file = file)
-  ttest
+  ttest.regions(r1, r2)
 })
-# names(ttest.all) <- colnames(regionpairs)
-save(ttest.all, file = "Resources/ttest.all_gtex_norm.RData")
+save(ttest.all, file = "Resources/ttest.all_gtex.RData")
 
 # Number of diff. genes
 diffGenes <- lapply(ttest.all, function(rp){
@@ -64,19 +78,37 @@ sapply(diffGenes, nrow)
 load("resources/braakGenes.RData")
 library(biomaRt)
 ensembl <- useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl", version=91)
-id_conversion <- lapply(braakGenes, function(x){
+id_conversion <- lapply(braakGenes, function(x){ # Get ensemble ID and HGNC gene symbol
   getBM(c('ensembl_gene_id', 'entrezgene', 'hgnc_symbol'), filters=c('entrezgene'), mart=ensembl, values=x)
 })
-braak_gtex <- sapply(diffGenes, function(rp){
-  sapply(id_conversion, function(x){
-    ens <- x$ensembl_gene_id
-    overlap <- sapply(ens, function(id){
-      sum(grepl(id, row.names(rp)))
-    })
-    sum(overlap)
-  })
+id_conversion <- lapply(id_conversion, function(x){ # Add AHBA gene symbols
+  x$AHBAsymbol <- entrezId2Name(x$entrezgene)
+  x
 })
-  
-  
 
+braak_ensembl <- lapply(id_conversion, function(x){ # For + and - correlated progression genes
+  ens <- x$ensembl_gene_id
+  intersect(ens, genes)
+})
 
+# Correct p-value for number of progression genes
+braak_gtex <- lapply(ttest.all, function(rp){
+  sapply(names(id_conversion), function(r){ # For + and - correlated progression genes
+    x <- id_conversion[[r]]
+    ens <- x$ensembl_gene_id
+    rows <- intersect(ens, rownames(rp))
+    t <- rp[rows,]
+    t$BH <- p.adjust(t$pvalue)
+    if (r== "positive_r") t[t$meanDiff < 2 & t$BH < 0.05, ]
+    else t[t$meanDiff > 2 & t$BH < 0.05, ]
+  }, simplify = FALSE)
+})
+sapply(braak_gtex, function(rp)sapply(rp,  nrow))
+gtex_pos <- lapply(braak_gtex, function(x){
+  rownames(x$positive_r)
+})
+gtex_neg <- lapply(braak_gtex, function(x){
+  rownames(x$negative_r)
+})
+length(Reduce(union, gtex_pos[1:3]))
+length(Reduce(union, gtex_neg[c(1,3)]))
