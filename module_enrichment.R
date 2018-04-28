0fbb2064b3524d508ebfe0c32380ef2ff10345f1
arlinkeo <arlinkeo@gmail.com>
2018-04-28 18:20:08 +0200

    other cell types disease associated genes

--- module_enrichment.R
+++ module_enrichment.R
@@ -34,7 +34,7 @@ hyper.test <- function(a, b){
 
 # Braak label correlation plot
 m <- modules[["braak1-6"]]
-names(m) <- paste0("M", names(m))
+names(m) <- rownames(labelCor)
 # module_size <- sapply(m, length)
 # tab <- data.frame(module = names(m), module_size)
 # tab$r <- labelCor$r
@@ -73,31 +73,63 @@ ensembl <- useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl", versio
 go_genes <- lapply(goterms, function(go){
   as.character(unlist(getBM(c('entrezgene'), filters='go', mart=ensembl, values=go)))
 })
-sapply(go_genes, length)
-
-go_genes <- lapply(go_genes, function(x){
-  x[x %in% ahba.genes()]
-})
-sapply(go_genes, length)
+names(go_genes) <- sapply(names(go_genes), function(x) paste0(toupper(substr(x, 1, 1)), substr(x, 2, nchar(x))))
 
 # Genes from studies
-studies <- list(jansen2017 = c("INPP5F", "TMEM175", "ASH1L", "MAPT", "RIT1", "C14orf83", "STK39", "GPNMB", "BST1", 
+studies <- list('Jansen et al. 2017' = c("INPP5F", "TMEM175", "ASH1L", "MAPT", "RIT1", "C14orf83", "STK39", "GPNMB", "BST1", 
                                 "SIPA1L2", "DLG2", "NUCKS1", "GCH1", "MCCC1", "FAM47E", "BCKDK", "TMPRSS9", "UBOX5", 
                                 "CCDC62", "SYNJ1", "EIF4G1", "FBXO7", "C20orf30", "POLG", "VPS13C", "PLA2G6"),
-                chang2017 = read.table("chang2017_riskgenes.txt", comment.char = "#", sep = "\n", row.names = NULL, stringsAsFactors = FALSE)[, 1], 
-                nalls2014 = read.table("nalls2014_riskgenes.txt", comment.char = "#", sep = "\n", row.names = NULL, stringsAsFactors = FALSE)[, 1]#,
+                'Chang et al. 2017' = read.table("chang2017_riskgenes.txt", comment.char = "#", sep = "\n", row.names = NULL, stringsAsFactors = FALSE)[, 1], 
+                'Nalls et al. 2014' = read.table("nalls2014_riskgenes.txt", comment.char = "#", sep = "\n", row.names = NULL, stringsAsFactors = FALSE)[, 1]#,
                 # liscovitch2014 = read.table("ifn_signaling_genes.txt", comment.char = "#", sep = "\n", row.names = NULL, stringsAsFactors = FALSE)[, 1]
 )
 studies <- lapply(studies, name2EntrezId)
-studies <- lapply(studies, function(x){
-  x[x %in% ahba.genes()]
-})
-sapply(studies, length)
+studies <- lapply(studies, function(x) x[!is.na(x)])
+
+# Disease-associated genes
+disease_table <- read.delim("gene_associated_with_disease.txt")
+diseases <- c("Parkinson Disease", "Young onset Parkinson disease", 
+              # "Autosomal Dominant Parkinsonism", "Dementia in Parkinson's disease", "Parkinsonian Disorders",   
+              # "Autosomal dominant late onset Parkinson disease", "Autosomal Recessive Parkinsonism", 
+              # "Parkinson Disease 6, Autosomal Recessive Early-Onset", "Parkinsonism, Juvenile", "Parkinson Disease, Familial, Type 1",
+              # "Parkinsonism-Dystonia, Infantile", "Secondary Parkinson Disease", "Wolff-Parkinson-White Syndrome",
+              "Alzheimer Disease, Late Onset", "Alzheimer Disease, Early Onset", "Alzheimer's Disease", 
+              "Dementia", "Lewy Body Disease", "Schizophrenia", "Lysosomal Storage Diseases", "Amyotrophic Lateral Sclerosis",
+              "Multiple System Atrophy", "Multiple Sclerosis", "Amyotrophic Lateral Sclerosis", 
+              "Huntington Disease", "Gastrointestinal Diseases", "Anemia", 
+              "Autoimmune Diseases", "Inflammation", "Prion Diseases",
+              "Muscle Rigidity", "Dystonia", "Bradykinesia", "Mental Depression")
+disease_genes <- sapply(diseases, function(x){
+  disease_table[disease_table$diseaseName %in% x, "geneId"]
+}, simplify = FALSE)
+
+# Cell-type genes
+celltype_genes <- sapply(c("Astrocytes", "Endothelial_cells", "Microglia", "Neurons", "Oligodendrocytes"), function(type){
+  file = paste0("brainscope_celltypes/", type, ".txt")
+  as.character(read.csv(file, header = TRUE)$entrez_id)
+}, simplify = FALSE)
+names(celltype_genes) <- paste0("BS_", names(celltype_genes))
+
+celltype_genes2 <- as.list(read.delim("celltype_markers_Ritchie2018.txt", na.strings = ""))
+celltype_genes2 <- lapply(celltype_genes2, toupper)
+celltype_genes2 <- lapply(celltype_genes2, name2EntrezId)
+celltype_genes2 <- lapply(celltype_genes2, function(x) x[!is.na(x)])
 
 # Combine all gene lists
-genelists <- Reduce(append, list(list(progression = unlist(braakGenes, use.names = FALSE)), go_genes, celltype_genes, studies))
+genelists <- Reduce(append, list(list('Progression genes' = unlist(braakGenes, use.names = FALSE)), 
+                                 go_genes, celltype_genes, studies, disease_genes))
+data.frame(sapply(genelists, length))
+
+# Filter for genes present in AHBA
+genelists <- lapply(genelists, function(x){
+  x[x %in% ahba.genes()]
+})
+data.frame(sapply(genelists, length))
+
 names(genelists) <- paste0(names(genelists), " (", sapply(genelists, length), ")")
 
+
+# Overlap with hypergeometrix test
 overlap <- sapply(names(genelists), function(n){
   type_genes <- genelists[[n]]
   pvalue <- sapply(m, function(mod_genes){
@@ -106,7 +138,6 @@ overlap <- sapply(names(genelists), function(n){
   pvalue <- p.adjust(pvalue, method = "BH") # corrected
   pvalue
 })
-# rownames(overlap) <- paste0("M", rownames(overlap))
 colnames(overlap) 
 overlap <- overlap[orderEG, ]
 overlap <- overlap[labelCor$pvalue[orderEG] < 0.001, ]
@@ -118,13 +149,14 @@ overlap$r <- sapply(overlap$Var1, function(m){
   cor <- labelCor[as.character(m), "r"]
   if (cor > 0) "r>0" else "r<0"
 })
-# overlap$r <- factor(overlap$r, levels = rev(unique(overlap$r)))
+overlap$Var2 <- factor(overlap$Var2, levels = rev(unique(overlap$Var2)))
 overlap$type <- sapply(overlap$Var2, function(x){
   x <- unlist(strsplit(as.character(x), split = " \\("))[1]
-  if (x == "progression") 1
+  if (x == "Progression genes") 1
   else if (x %in% names(celltype_genes)) 2
-  else if (x %in% names(goterms)) 3
+  else if (x %in% names(go_genes)) 3
   else if (x %in% names(studies)) 4
+  else if (x %in% names(disease_genes)) 5
   else 0
 })
 
@@ -134,7 +166,7 @@ p2 <- ggplot(overlap) +
   facet_grid(type~r, scales = "free", space = "free")
 p2
 
-pdf("module_enrichment.pdf", 10, 4)
+pdf("module_enrichment.pdf", 9, 6)
 # p1
 p2
 dev.off()
