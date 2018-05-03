ff5f7d75ede758c46119dd145c75fd512a5121ce
arlinkeo <arlinkeo@gmail.com>
2018-05-03 11:21:38 +0200

    plot

--- heatmap_expr_eigengenes.R
+++ heatmap_expr_eigengenes.R
@@ -14,6 +14,10 @@ load("resources/summaryLabelCorrEG.RData")
 labelCor <- do.call(rbind.data.frame, lapply(summaryLabelCorrEG, function(g) g["summary",]))
 
 # heatmap
+eigenExpr <- lapply(eigenExpr, function(m){
+  rownames(m) <- paste0("M", rownames(m))
+  m
+})
 
 # IDs and AHBA colors for each sample per donor
 sampleInfo <- lapply(donorNames, function(d){
@@ -26,35 +30,39 @@ sampleInfo <- lapply(donorNames, function(d){
 })
 
 modules <- names(modules[["braak1-6"]])
-rowOrder <- order(-labelCor$r)
+rowOrder <- order(labelCor$r)
 colPal <- c("darkblue", "white", "darkred")
-rampcols <- colorRampPalette(colors = colPal, space="Lab")(200)
-  
-pdf("heatmap_expr_eigengenes.pdf", 8, 6)
+rampcols <- colorRampPalette(colors = colPal, space="Lab")(201)
+rowColor <- rampcols[as.numeric(cut(labelCor$r, breaks = 201))][rowOrder]
+rowsep <- which(labelCor$r[rowOrder] > 0)[1]
 
+pdf("heatmap_expr_eigengenes.pdf", 8, 6)
 lapply(donorNames, function(d){
   # Subselect expression matrices
   samples <- braakStages[[d]]
-  # samples <- as.logical(bitwOr(samples[, "braak1"], samples[, "braak6"]))
-  # samples <- as.logical(samples[, b])
   samples <- as.logical(apply(samples, 1, sum))
   df <- sampleInfo[[d]][samples,]
   labels <- braakLabels[[d]][samples]
   exprMat <- as.matrix(eigenExpr[[d]])
+  colColor <- df$color_hex_triplet
   
   colOrder <- order(labels, -df$graph_order)
   df <- df[colOrder, ]
   exprMat <- exprMat[rowOrder, colOrder]
+  # rownames(exprMat)[seq(2, nrow(exprMat), by= 3)] <- paste0("            ", rownames(exprMat)[seq(2, nrow(exprMat), by= 3)])
+  # rownames(exprMat)[seq(3, nrow(exprMat), by= 3)] <- paste0("                        ", rownames(exprMat)[seq(3, nrow(exprMat), by= 3)])
+  rownames(exprMat)[-which(rownames(exprMat) %in% c("M85", "M10", "M2", "M48"))] <- "" 
+  colColor <- colColor[colOrder]
   
-
   heatmap.2(exprMat, col = rampcols, 
-            labCol = df$name, 
+            labCol= df$name, 
+            rowsep = rowsep, sepcolor = "black",
             Rowv=FALSE, Colv=FALSE, 
-            cexCol = 0.1, cexRow = 0.5,
+            cexCol = 0.1, cexRow = 1,
             scale = "none", trace = "none", dendrogram = "none", key = FALSE, 
-            ColSideColors = df$color_hex_triplet,
-            main = paste0("Expression of modules in Braak 1-6"),
-            margins = c(15, 5))
+            RowSideColors = rowColor, ColSideColors = colColor,
+            main = d,
+            margins = c(15, 10))
+  
 })
-
 dev.off()
