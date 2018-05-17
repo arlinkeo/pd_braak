53a397deb4cbc518e5bf24c245ff1facdabd7fcb
arlinkeo <arlinkeo@gmail.com>
2018-05-18 00:37:36 +0200

    color change

--- volcanoplot_summary_diff.R
+++ volcanoplot_summary_diff.R
@@ -1,5 +1,4 @@
 # Volcano plot for differential expression analysis
-
 setwd("C:/Users/dkeo/surfdrive/pd_braak")
 source("PD/base_script.R")
 
@@ -7,8 +6,11 @@ library(ggplot2)
 library(gridExtra)
 library(ggrepel)
 load("resources/braakGenes.RData")
-braakGenes <- unlist(braakGenes)
+# braakGenes <- unlist(braakGenes)
 load("resources/summaryDiffExpr.RData")
+
+braak_pos <- braakGenes$entrez_id[braakGenes$braak_r>0]
+braak_neg <- braakGenes$entrez_id[braakGenes$braak_r<0]
 
 # Extract summary statistics
 diffExpr <- lapply(summaryDiffExpr, function(rp){
@@ -34,11 +36,16 @@ diffExpr <- lapply(diffExpr, function(tab){
 ctab <- Reduce(rbind, diffExpr)
 xmax <- max(ctab$meanDiff)
 xmin <- min(ctab$meanDiff)
-ymax <- ceiling(max(ctab$'logp'[is.finite(tab$'logp')]))
+ymax <- ceiling(max(ctab$'logp'[is.finite(ctab$'logp')]))
 
 plotll <- lapply(names(diffExpr), function(rp){
   tab <- diffExpr[[rp]]
-  tab$info <- as.numeric(rownames(tab) %in% braakGenes)
+  # tab$info <- as.numeric(rownames(tab) %in% braak_pos)
+  tab$info <- sapply(rownames(tab), function(x){
+    if (x %in% braak_pos) 1
+    else if (x %in% braak_neg) 2
+    else 0
+  })
   
   # tab$labels <- entrezId2Name(rownames(tab))
   # pd <- c("DNAJC13","SNCA","GCH1","INPP5F", "ASH1L", "ITPKB", "ELOVL7", "ZNF184", "SCARB2", "DDRGK1")
@@ -52,7 +59,7 @@ plotll <- lapply(names(diffExpr), function(rp){
 
   p <- ggplot(tab, aes(meanDiff, logp, colour = info)) +
     geom_point(size = 0.25, alpha = 0.3) +
-    scale_colour_manual(values = c("0"="#F8766D", "1"="#00BFC4", "2"="black")) +
+    scale_colour_manual(values = c("0"="grey", "1"="red", "2"="blue")) +
     # geom_text_repel(label = tab$labels, fontface = "italic", colour = "black", size = 3, nudge_x = 0.2) +
     labs(x = "fold-change", y = "-log10 p-value") +
     scale_x_continuous(limits = c(xmin, xmax), expand = c(0,0)) +
@@ -61,11 +68,7 @@ plotll <- lapply(names(diffExpr), function(rp){
     theme
   
   name <- paste0("DiffExpr_braak/volcano_ttest/volcanoplot_", rp, ".pdf")
-  pdf(file = name, 4, 3)
+  pdf(file = name, 3, 2)
   print(p)
   dev.off()
 })
-# 
-# pdf(file = "volcanoplot_summary_diff.pdf", 12, 12)
-# grid.arrange(grobs = plotll, top = "Summary differential expression", nrow = 5)
-# dev.off()
