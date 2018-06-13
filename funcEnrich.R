c6ccc9a9697d369fc5386a238be31c46861b9ca9
arlinkeo <arlinkeo@gmail.com>
2018-06-13 18:59:45 +0200

    braak genes stored differently

--- funcEnrich.R
+++ funcEnrich.R
@@ -7,23 +7,24 @@ load("resources/modules.RData")
 load( "resources/braakGenes.RData")
 # R version should be 3.25 instead of 3.3 for RDavid
 library("RDAVIDWebService")
-# library("plyr")
 
-############################################################################
+braak <- lapply(c(positive = "pos", negative = "neg"), function(x){
+  braakGenes$entrez_id[braakGenes$dir %in% x]
+})
 
 #Functional enrichment of  genes correlated greater or smaller than 0
 david<-DAVIDWebService$new(email="D.L.Keo@tudelft.nl",
                            url="https://david.abcc.ncifcrf.gov/webservice/services/DAVIDWebService.DAVIDWebServiceHttpSoap12Endpoint/")
 setAnnotationCategories(david, c("GOTERM_BP_ALL", "GOTERM_MF_ALL", "GOTERM_CC_ALL"))
-bg_list <- probeInfo$entrez_id
+bg_list <- ahba.genes()
 bg <- addList(david, bg_list, idType = "ENTREZ_GENE_ID", listName = "AHBA background", listType = "Background")
 bg
 t <- 0.05 # EASE p-value threshold
 setTimeOut(david, 200000)
 
 # Enrichment of positively and negatively correlated progression genes
-lapply(names(braakGenes), function(r){
-  genes <- braakGenes[[r]]
+lapply(names(braak), function(r){
+  genes <- braak[[r]]
   result <- addList(david, genes, idType = "ENTREZ_GENE_ID", listName = r, listType = "Gene")
   print(result)
   setCurrentBackgroundPosition(david, 1)
