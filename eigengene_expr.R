dc58a08b9f78bee7444f341cd8a003ff97d6da5f
arlinkeo <arlinkeo@gmail.com>
2018-06-14 18:05:36 +0200

    merged

--- eigengene_expr.R
+++ eigengene_expr.R
@@ -2,6 +2,7 @@
 setwd("C:/Users/dkeo/surfdrive/pd_braak")
 library(ggplot2)
 library(ggrepel)
+library(gplots)
 source("PD/base_script.R")
 load("../ABA_Rdata/BrainExprNorm.RData")
 load("resources/modules.RData")
@@ -62,7 +63,7 @@ tab <- labelCor
 tab$BH <- p.adjust(tab$pvalue, method = "BH")
 tab$'logp' <- -log10(tab$BH)
 mod_pos <- rownames(tab)[tab$BH < 0.001 & tab$r > 0] # significant correlated modules
-mod_neg <- rownames(tab)[tab$BH < 0.001 & tab$r < 0] # significant correlated modules
+eg <- rownames(tab)[tab$BH < 0.001 & tab$r < 0] # significant correlated modules
 tab$info <- sapply(rownames(tab), function(m){
   if (m %in% mod_neg) 1
   else if (m %in% mod_pos) 2
@@ -90,6 +91,56 @@ p <- ggplot(tab, aes(r, logp, colour = info)) +
   scale_y_continuous(limits = c(0, ymax), expand = c(0,0)) +
   theme
 p
-pdf("eigengene_r_volcanoplot.pdf", 6, 4.5)
+pdf("eigengene_r_volcanoplot.pdf", 5, 4.5)
 p
+dev.off()
+
+##############################################################################################
+
+# Heatmap Expression of eigen gene
+ontology <- read.csv("../ABA_human_processed/Ontology_edited.csv")
+
+# IDs and AHBA colors for each sample per donor
+sampleInfo <- lapply(donorNames, function(d){
+  sampleIds <- read.csv(paste("../ABA_human_processed/sample_info_normalized_microarray_", d, "_2014-11-11.csv", sep = ""))[ , 1]
+  info <- ontology[match(sampleIds, ontology$id), ]
+  info$color_hex_triplet <- sapply(info$color_hex_triplet, function(c){
+    if (nchar(c) == 5) {paste("#0", c, sep = "")} else {paste("#", c, sep = "")}
+  })
+  info
+})
+
+# modules <- names(modules[["braak1-6"]])
+colOrder <- order(labelCor$r)
+colPal <- c("darkblue", "white", "darkred")
+rampcols <- colorRampPalette(colors = colPal, space="Lab")(201)
+colColor <- rampcols[as.numeric(cut(labelCor$r, breaks = 201))][colOrder]
+colsep <- which(labelCor$r[colOrder] > 0)[1]
+
+pdf("heatmap_expr_eigengenes.pdf", 8, 6)
+lapply(donorNames, function(d){
+  # Subselect expression matrices
+  samples <- braakLabels[[d]] != 0
+  df <- sampleInfo[[d]][samples,]
+  exprMat <- as.matrix(eigenExpr[[d]])
+  rowColor <- df$color_hex_triplet
+  
+  rowOrder <- order(labels[[d]], -df$graph_order)
+  df <- df[rowOrder, ]
+  exprMat <- t(exprMat[colOrder, rowOrder])
+  colnames(exprMat) <- NULL
+  rowColor <- rowColor[rowOrder]
+  rowsep <- match(c(2:6), labels[[d]][rowOrder])# separate Braak regions
+  
+  heatmap.2(exprMat, col = rampcols, 
+            labRow = df$name, 
+            rowsep = rowsep, colsep = colsep, sepcolor = "red",
+            Rowv=FALSE, Colv=FALSE, 
+            cexCol = 1, cexRow = .1,
+            scale = "none", trace = "none", dendrogram = "none", key = FALSE, 
+            RowSideColors = rowColor, ColSideColors = colColor,
+            main = d,
+            margins = c(5, 20))
+  
+})
 dev.off()
