3f953fe138fd3d03c14f67da1cadf4a018c3fece
arlinkeo <arlinkeo@gmail.com>
2018-06-14 18:06:40 +0200

    rm

--- main.R
+++ main.R
@@ -23,7 +23,6 @@ source("PD/boxplot_UKBEC.R")
 # Braak co-expression modules
 source("PD/coexpr_modules.R")
 source("PD/eigengene_expr.R")
-source("PD/module_expr_eigengenes.R")
 
 source("PD/funcEnrich.R")
 source("PD/module_enrichment.R")
