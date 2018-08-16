92c724009960361320a7c6ad6646f5637aff2527
arlinkeo <arlinkeo@gmail.com>
2018-08-16 11:30:26 +0200

    correlation of fold-changes

--- braaklabel_cor.R
+++ braaklabel_cor.R
@@ -2,15 +2,35 @@
 setwd("C:/Users/dkeo/surfdrive/pd_braak")
 source("PD/base_script.R")
 library(metafor)
-
 load("resources/braakInfo.RData")
+load("resources/summaryCoef.RData")
+
+##### Correlation on fold-changes #####
+
+# Convert lists: gene -> braak coef
+
+fc_cor <- as.data.frame(t(sapply(ahba.genes(), function(g){
+  estimates <- sapply(braakNames[-1], function(b){
+    unlist(summaryCoef[[b]][[g]]["summary", "Estimate"])
+  })
+  fc <- c(braak1 = 0, estimates)
+  r <- cor.test(fc, c(1:6))
+  c(r = unname(r$estimate), pvalue = r$p.value)
+  # plot(c(1:6), fc)
+})))
+fc_cor$BH <- p.adjust(fc_cor$r, method = "BH")
+save(fc_cor, file = "resources/fc_cor.RData")
+
+##### Correlation on expression data #####
+
 labels <- lapply(braakLabels, function(x) x[x != 0] )# All Braak labels
 
 summary.braak.cor <- dget("PD/summary.braak.cor.R")
 
 # For all genes
 samples <- lapply(braakLabels, function(x) x != 0)
-expr <- readRDS("resources/expr_neuroncorrected.rds")
+expr <- readRDS("../ABA_Rdata/BrainExprNorm.rds")#"resources/expr_neuroncorrected.rds")
+
 expr_braak <- lapply(donorNames, function(d){
   e <- expr[[d]]
   labels <- samples[[d]]
