994d732720d53fd436e9f90283debf8aaa97c6d2
arlinkeo <arlinkeo@gmail.com>
2018-08-29 18:15:24 +0200

    title handles array data

--- plot.ref.signal.R
+++ plot.ref.signal.R
@@ -1,22 +1,17 @@
 # Plot reference signal
 
-plot.ref.signal <- function(data){
+plot.ref.signal <- function(data, title, vline = NULL){
   colnames(data) <- make.names(colnames(data), unique = TRUE)
   
-  df <- melt(as.matrix(expr))
+  df <- melt(data)
   colnames(df) <- c("celltype", "sample", "expr")
   df$sample <- factor(df$sample, levels = unique(df$sample))
-  
-  intercepts <- match(c(1:6), braakLabels[[d]][idx])[-1]
   
   ggplot(df, aes(x=sample, y=expr, color=celltype)) + 
     geom_point() +
     geom_smooth(aes(group=celltype)) +
-    geom_vline(xintercept = intercepts) +
-    ggtitle(d) +
-    theme(panel.background = element_blank(), 
-          panel.grid = element_blank(), 
-          axis.line = element_line(colour = "black"),
-          axis.text.x = element_blank(),
+    geom_vline(xintercept = vline) +
+    ggtitle(title) + theme_classic() + 
+    theme(axis.text.x = element_blank(),
           axis.ticks.x = element_blank())
 }
