db45419850a606ad38b60aaf346458a8e2becbbe
arlinkeo <arlinkeo@gmail.com>
2018-09-20 15:03:46 +0200

    comment

--- main.R
+++ main.R
@@ -19,7 +19,7 @@ source("PD/boxplot_braak.R")
 # Gene conversion table
 source("ahba_entrez2ensembl.R")
 
-# Validate progression genes
+# Validate Braak stage related genes
 source("PD/diff_expr_GTEX.R")
 source("PD/ukbecGeneID.R")
 source("PD/diff_expr_UKBEC.R")
@@ -30,6 +30,3 @@ source("PD/gene_coexpr.R")
 source("PD/coexpr_modules.R")
 source("PD/eigengene_cor.R")
 source("PD/module_enrichment.R")
-# source("PD/")
-
-# source("PD/")
