52e4736fe61138ff7c5f6a5c15326edd429c8ab2
arlinkeo <arlinkeo@gmail.com>
2018-10-24 17:29:34 +0200

    moved to base script

--- braakgenes.R
+++ braakgenes.R
@@ -115,17 +115,6 @@ print(p)
 dev.off()
 
 ########## Presence of PD-implicated genes ##########
-pdGenes <- list(hiImpact = c("SNCA", "LRRK2", "GBA", "VPS35", "PARK2", "UCHL1", "PINK1", "PARK7", "ATP13A2", "PLA2G6", "FBXO7", "DNAJC6", "SYNJ1", 
-                             "EIF4G1", "DNAJC13", "CHCHD2", "C20orf30", "RIC3", "LRP10"), #TMEM230 is C20orf30
-                jansen2017 = c("INPP5F", "TMEM175", "ASH1L", "MAPT", "RIT1", "C14orf83", "STK39", "GPNMB", "BST1", 
-                               "SIPA1L2", "DLG2", "NUCKS1", "GCH1", "MCCC1", "FAM47E", "BCKDK", "TMPRSS9", "UBOX5", 
-                               "CCDC62", "SYNJ1", "EIF4G1", "FBXO7", "C20orf30", "POLG", "VPS13C", "PLA2G6"),
-                hla = c("HLA-DRA", "HLA-DRB1", "HLA-DRB5", "HLA-DQB1"),
-                'Chang et al. 2017' = read.table("chang2017_riskgenes.txt", comment.char = "#", sep = "\n", row.names = NULL, stringsAsFactors = FALSE)[, 1], 
-                'Nalls et al. 2014' = read.table("nalls2014_riskgenes.txt", comment.char = "#", sep = "\n", row.names = NULL, stringsAsFactors = FALSE)[, 1],
-                parkingenes = read.table("parkin_genes_hgnc.txt", sep = "\t", header = TRUE)$Approved.Symbol
-)
-pdGenesID <- lapply(pdGenes, name2EntrezId)
 tab <- lapply(names(pdGenesID), function(n){
   x <- pdGenesID[[n]]
   g <- intersect(braakGenes$entrez_id, x)
