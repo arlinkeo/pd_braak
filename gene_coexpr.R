1617c35a1df35a3c9e0d24935bb98817f5d014be
arlinkeo <arlinkeo@gmail.com>
2018-10-24 17:23:35 +0200

    gene coexpr only for whole braak region

--- gene_coexpr.R
+++ gene_coexpr.R
@@ -1,34 +1,22 @@
 # Gene co-expression in whole Braak region
 setwd("/tudelft.net/staff-bulk/ewi/insy/DBL/Arlin/pd_braak")
+load("../ABA_Rdata/BrainExpr.RData")
 
-source("PD/base_script.R")
-
-# Random subselection of genes
-genes <- ahba.genes()
+# Brain donor names
+donorNames <- c("donor9861", "donor10021", "donor12876", "donor14380", "donor15496", "donor15697")
+names(donorNames) <- donorNames
 
 # Labels for whole Braak region
-load("resources/braakLabels.RData") # Braak stage label vectors
+load("resources/braakInfo.RData") # Braak stage label vectors
 wholeBraak <- lapply(braakLabels, function(d){ d != 0 })# Merge labels 1-6
+sapply(wholeBraak, sum)
 
-# Labels for non-Braak region
-load("resources/nonBraak.RData") # Braak stage label vectors
-nonBraak <- nonBraak$nonBraakA2 # all except braak and cerebellum samples
-
-# Labels for each Braak region 
-load("resources/braakStages.RData")
-braakStages <- lapply(braakNames, function(b){
-  lapply(braakStages, function(d) unlist(d[, b]))
+# Average co-expression for whole region
+gene_coexpr <- lapply(donorNames, function(d){
+  print(d)
+  e <- brainExpr[[d]][, wholeBraak[[d]]]
+  cor(t(e), method = "pearson") #r-matrix
 })
-
-# Combine all labels of regions in list
-sampleList <- c(braakStages, 'braak1-6' = list(wholeBraak), nonbraak = list(nonBraak))
-
-# Average co-expression for all regions in sampleList
-lapply(names(sampleList), function(n){
-  r <- sampleList[[n]]
-  expr <- select.expr(genes, r)
-  gene_coexpr <- gene.coexpr(expr)
-
-  avgCoexpr <- apply(simplify2array(gene_coexpr), 1:2, mean)
-  saveRDS(avgCoexpr, file = paste0("resources/avgCoexpr_", n, ".rds"))
-})
+gene_coexpr <- simplify2array(gene_coexpr) # genes x genes x donors
+avgCoexpr <- apply(gene_coexpr, 1:2, mean)
+saveRDS(avgCoexpr, file = "resources/avgCoexpr_wholeBraak.rds")
