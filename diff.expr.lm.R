bd562bdaedaed8e51d689528e957a4f53684bb6a
arlinkeo <arlinkeo@gmail.com>
2018-11-07 16:35:34 +0100

    Correct P only across genes (not regions)

--- diff.expr.lm.R
+++ diff.expr.lm.R
@@ -12,9 +12,14 @@ diff.expr.lm <- function(data, ref, group){
     rownames(t) <- fit$xlevels$group[-1]
     t
   })
-  arr <- simplify2array(tab_list) # 3D-array
-  df <- melt(arr[, "Pr(>|t|)", ])
-  df$value <- p.adjust(df$value, method = "BH") # P-value corrected for genes and braak regions
-  bh <- dcast(df, Var1 ~ Var2)[, -1]
-  abind(arr, 'BH' = bh, along = 2) # Returns 3D-array: groups x measures x rows (genes)
+  arr <- simplify2array(tab_list) # 3D-array: groups x measures x genes
+  arr <- aaply(arr, 1, function(t){ # Correct P for number of genes (per group)
+    bh <- p.adjust(t["Pr(>|t|)", ], method = "BH")
+    rbind(t, 'BH' = bh)
+  })
+  # df <- melt(arr[, "Pr(>|t|)", ])
+  # df$value <- p.adjust(df$value, method = "BH") # P-value corrected for genes and braak regions
+  # bh <- dcast(df, Var1 ~ Var2)[, -1]
+  # arr <- abind(arr, 'BH' = bh, along = 2) # Returns 3D-array: groups x measures x rows (genes)
+  arr
 }
