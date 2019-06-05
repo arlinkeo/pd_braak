d9ec8f8d7aefed3dc7733da415f73894cbe4bd01
arlinkeo <arlinkeo@gmail.com>
2019-06-05 18:58:14 +0200

    summary to Summary

--- forestplot.R
+++ forestplot.R
@@ -49,6 +49,7 @@ theme.grid <- theme(panel.background = element_blank(), panel.grid = element_bla
 
 tab <- data.frame(summaryDiffExpr[geneId, "R1-R6", , ])
 tab$donors <- gsub("donor", "Donor ", rownames(tab))
+tab$donors[7] <- "Summary"
 tab$donors <- factor(tab$donors, levels = unique(rev(tab$donors)))
 # Side table info
 tab$rmd <- paste0(round(tab$Estimate, digits = 2), " (", round(tab$lower95, digits = 2), ", ", round(tab$upper95, digits = 2), ")")
@@ -61,15 +62,12 @@ fp1 <- ggplot(data = tab, aes(Estimate, donors)) +
   geom_point(aes(size = weight, shape = is.summary, color = is.summary)) +
   geom_errorbarh(aes(xmin = lower95, xmax = upper95), height = 0) +
   geom_vline(xintercept = 0, linetype = "dashed") +
-  # geom_vline(xintercept = -1, linetype = "dotted") +
   scale_shape_manual(values = c(15,18)) +
   scale_color_manual(values = c('#00CCCC', '#FF8000')) +
   labs(title = bquote(italic(.(gene))), x = "FC") +
   scale_y_discrete(labels = rev(tab$donor)) +
-  # scale_x_continuous(limits = c(x.min, x.max)) +
-  geom_text(aes(x =  3, label = rmd), size = 3) +
-  geom_text(aes(x = 4, label = weight), size = 3) +
-  # geom_text(data = colLabels, aes(x, y, label = label)) +
+  # geom_text(aes(x =  3, label = rmd), size = 3) +
+  # geom_text(aes(x = 4, label = weight), size = 3) +
   theme.grid + theme(plot.margin = unit(c(0,4,0,4), "lines"))
 
 ##########################################
@@ -86,17 +84,14 @@ fp2 <- ggplot(data = tab, aes(r, donors)) +
   geom_point(aes(size = weight, shape = is.summary, color = is.summary)) +
   geom_errorbarh(aes(xmin = lower95, xmax = upper95), height = 0) +
   geom_vline(xintercept = 0, linetype = "dashed") +
-  # geom_vline(xintercept = -1, linetype = "dotted") +
   scale_shape_manual(values = c(15,18)) +
   scale_color_manual(values = c('#00CCCC', '#FF8000')) +
   labs(title = bquote(italic(.(gene))), x = "Correlation") +
   scale_y_discrete(labels = rev(tab$donor)) +
-  # scale_x_continuous(limits = c(x.min, x.max)) +
-  geom_text(aes(x = 1.6, label = rci), size = 3) +
-  geom_text(aes(x = 2.2, label = weight), size = 3) +
-  # geom_text(data = colLabels, aes(x, y, label = label)) +
+  # geom_text(aes(x = 1.6, label = rci), size = 3) +
+  # geom_text(aes(x = 2.2, label = weight), size = 3) +
   theme.grid + theme(plot.margin = unit(c(0,4,0,4), "lines"))
-pdf(file = paste0("forestplot_", gene, "_2.pdf"), 8,2)
+pdf(file = paste0("forestplot_", gene, ".pdf"), 4, 2)
 fp1
 fp2
 dev.off()
