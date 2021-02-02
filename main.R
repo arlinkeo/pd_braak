7e30d542d05075a687414679aba243d0e189ba69
arlinkeo <d.keo-denboer@erasmusmc.nl>
2021-02-02 18:08:29 +0100

    repository directory name

--- main.R
+++ main.R
@@ -16,42 +16,42 @@ R.utils::sourceDirectory(fun_dir, modifiedOnly = FALSE)
 dir.create("output")
 
 # AHBA Data preprocessing
-source("PD/probe2gene_AHBA.R")
+source("pd_braak/probe2gene_AHBA.R")
 
 # Assign AHBA samples to Braak stage-related regions
-source("PD/braakStageSamples.R")
+source("pd_braak/braakStageSamples.R")
 
 # Select Braak stage-related genes (BRGs)
-source("PD/diff_expr_ahba.R")
-source("PD/braaklabel_cor.R")
-source("PD/braakgenes.R")
+source("pd_braak/diff_expr_ahba.R")
+source("pd_braak/braaklabel_cor.R")
+source("pd_braak/braakgenes.R")
 
 # Visualize BRGs in boxplots
-source("PD/boxplot_braak.R")
+source("pd_braak/boxplot_braak.R")
 
 # Gene conversion table for analysis of RNA-seq datasets
-source("ahba_entrez2ensembl.R")
+source("pd_braak/ahba_entrez2ensembl.R")
 
 # Validate Braak stage related genes in datasets of non-neurological controls
-source("PD/diff_expr_UKBEC.R")
-source("PD/diff_expr_GTEX.R")
+source("pd_braak/diff_expr_UKBEC.R")
+source("pd_braak/diff_expr_GTEX.R")
 
 # Validate Braak stage related genes in datasets of PD patients
-source("PD/probe2genes_PD_microarray.R")
-source("PD/diffexpr_PD_microarray.R")
-source("PD/diffexpr_PD_RNAseq.R")
+source("pd_braak/probe2genes_PD_microarray.R")
+source("pd_braak/diffexpr_PD_microarray.R")
+source("pd_braak/diffexpr_PD_RNAseq.R")
 
 # Braak co-expression modules
-source("PD/gene_coexpr.R") # Ran on HP server
-source("PD/coexpr_modules.R")
-source("PD/eigengene_cor.R")
-source("PD/module_enrichment.R")
+source("pd_braak/gene_coexpr.R") # Ran on HP server
+source("pd_braak/coexpr_modules.R")
+source("pd_braak/eigengene_cor.R")
+source("pd_braak/module_enrichment.R")
 
 # Presence of PD-implicated genes
-source("PD/pd_implicated_genes.R")
+source("pd_braak/pd_implicated_genes.R")
 
 # Other  scripts 
-source("PD/lineplot_braak.R")
+source("pd_braak/lineplot_braak.R")
 # forestplot.R
 
 # psea.R
